916574b8e9d96accbe21e52dac2a7e205dc14217
jiaoyangneal <yjiaoneal@163.com>
2015-09-21 17:17:22 +0800

    添加数据库方法

---
+++ financial_experiment/src/main/java/com/lcmf/rec/fundpool/FundFilter.java
@@ -0,0 +1,8 @@
+package com.lcmf.rec.fundpool;
+
+public interface FundFilter {
+
+	
+	public boolean filter();
+		
+}
---
+++ financial_experiment/src/main/java/com/lcmf/rec/fundpool/FundPool.java
@@ -0,0 +1,21 @@
+package com.lcmf.rec.fundpool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FundPool {
+
+	List<String> funds_code = new ArrayList<String>();
+	
+	
+	public FundPool(){
+		
+	}
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
