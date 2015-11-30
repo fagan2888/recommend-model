8d83c61b28c80350e9f3a8028db70b730d243670
yjiaoneal <yjiaoneal@163.com>
2015-11-30 11:27:03 +0800

    add export data and fund data project

---
+++ fund_data/shell/fund_data.py
@@ -0,0 +1,9 @@
+#coding=utf8
+
+
+import string
+import MySQLdb
+
+
+conn = MySQLdb.connect(host='rdsf4ji381o0nt6n2954.mysql.rds.aliyuncs.com', port=3306, user='data', passwd='oDCiT8OfKaHf2E6P2cR3', db='data', charset='utf8')
+
