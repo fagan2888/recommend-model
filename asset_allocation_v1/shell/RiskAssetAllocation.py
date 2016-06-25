3bbe473052bb9cff1077cb3dae321bdb41ac7551
jiaoyangneal <yjiaoneal@163.com>
2016-06-25 11:16:54 +0800

    fix bug

---
+++ asset_allocation_v1/shell/RiskAssetAllocation.py
@@ -0,0 +1,64 @@
+#coding=utf8
+
+
+
+import string
+import json
+import sys
+sys.path.append('shell')
+import pandas as pd
+import LabelAsset
+import EqualRiskAssetRatio
+import EqualRiskAsset
+import HighLowRiskAsset
+import os
+import DB
+import MySQLdb
+from datetime import datetime
+import AllocationData
+import time
+
+
+
+
+def risk_asset_allocation():
+
+
+	allocationdata = AllocationData.allocationdata()
+	allocationdata.all_dates()
+
+	#allocationdata.all_dates()
+
+	#allocationdata.start_date                            = args.get('start_date')
+	#allocationdata.start_date                            = '2010-01-01'
+	allocationdata.start_date                            = '2016-01-01'
+	allocationdata.fund_measure_lookback                 = 52
+	allocationdata.fund_measure_adjust_period            = 26
+	allocationdata.jensen_ratio                          = 0.5
+	allocationdata.sortino_ratio                         = 0.5	
+	allocationdata.ppw_ratio                             = 0.5		
+	allocationdata.stability_ratio                       = 0.5	
+	allocationdata.fixed_risk_asset_lookback             = 52	
+	allocationdata.fixed_risk_asset_risk_adjust_period   = 5	
+	allocationdata.allocation_lookback                   = 13 
+	allocationdata.allocation_adjust_period              = 13	
+
+
+
+	LabelAsset.labelasset(allocationdata)
+	EqualRiskAssetRatio.equalriskassetratio(allocationdata)
+	EqualRiskAsset.equalriskasset(allocationdata)
+	HighLowRiskAsset.highlowriskasset(allocationdata)
+
+
+
+	#DB.fund_measure(allocationdata)
+	#DB.label_asset(allocationdata)
+	#DB.asset_allocation(allocationdata)
+
+
+
+
+if __name__ == '__main__':
+
+	risk_asset_allocation()
