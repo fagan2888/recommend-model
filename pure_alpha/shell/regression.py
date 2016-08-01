b61b12fd127751b99ec2b9fae44d8cb53d1df176
jiaoyangneal <yjiaoneal@163.com>
2016-08-01 10:57:39 +0800

    添加max r平方

--- pure_alpha/shell/regression.py
+++ pure_alpha/shell/regression.py
@@ -1,54 +1,11 @@
 #coding=utf8
 
 
+
 import scipy
 import statsmodels.formula.api as smf
 import pandas as pd
-
-
-
-def forward_selected(data, response):
-
-
-    """Linear model designed by forward selection.
-
-    Parameters:
-    -----------
-    data : pandas DataFrame with all possible predictors and response
-
-    response: string, name of response column in data
-
-    Returns:
-    --------
-    model: an "optimal" fitted statsmodels linear model
-           with an intercept
-           selected by forward selection
-           evaluated by adjusted R-squared
-    """
-
-    remaining = set(data.columns)
-    remaining.remove(response)
-    selected = []
-    current_score, best_new_score = 0.0, 0.0
-    while remaining and current_score == best_new_score:
-        scores_with_candidates = []
-        for candidate in remaining:
-            formula = "{} ~ {} + 1".format(response,
-                                           ' + '.join(selected + [candidate]))
-            print formula
-            print data
-            score = smf.ols(formula, data).fit().rsquared_adj
-            scores_with_candidates.append((score, candidate))
-        scores_with_candidates.sort()
-        best_new_score, best_candidate = scores_with_candidates.pop()
-        if current_score < best_new_score:
-            remaining.remove(best_candidate)
-            selected.append(best_candidate)
-            current_score = best_new_score
-    formula = "{} ~ {} + 1".format(response,
-                                   ' + '.join(selected))
-    model = smf.ols(formula, data).fit()
-    return model
+import itertools
 
 
 
@@ -57,8 +14,35 @@ def stepwise_regression():
 
 
 
+def max_sq_r(fund_dfr, factor_dfr):
+
+	factors = factor_dfr.columns
+
+	for i in range(1, len(factors)):
+
+		print list(itertools.combinations(['a', 'b', 'c'], 2))
+
+
+
 if __name__ == '__main__':
 
+
+	factor_dfr = pd.read_excel('./data/factors.xlsx', index_col='Date')
+	#print factor_dfr
+
+	fund_dfr = pd.read_excel('./data/ls.xlsx', index_col='Date')
+	cols     = fund_dfr.columns
+
+	fund_dfr = fund_dfr[cols[0]]
+	print fund_dfr
+
+
+	#max_sq_r(None, None)
+
+
+	#print 'hehe'
+
+	'''
     url = "http://data.princeton.edu/wws509/datasets/salary.dat"
     data = pd.read_csv(url, sep='\\s+')
 
@@ -71,4 +55,4 @@ if __name__ == '__main__':
 
     print model.rsquared_adj
     # 0.835190760538
-
+	'''
--- pure_alpha/shell/svd.py
+++ pure_alpha/shell/svd.py
@@ -4,15 +4,17 @@
 from sklearn.decomposition import TruncatedSVD
 from sklearn.random_projection import sparse_random_matrix
 
+
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
 
 
-df = pd.read_csv('./data/index.csv', index_col = 'date', parse_dates= ['date'])
+df = pd.read_csv('./data/clean_funds.csv', index_col = 'date', parse_dates= ['date'])
+
 
 dfr = df.pct_change().fillna(0.0)
-dfr = dfr[['000905.SH','000300.SH','399006.SZ']]
-#print dfr
+#dfr = dfr[['000905.SH','000300.SH','399006.SZ']]
 
 
 #X = sparse_random_matrix(100, 100, density=0.01, random_state=42)
@@ -21,11 +23,37 @@ dfr = dfr[['000905.SH','000300.SH','399006.SZ']]
 
 svd = TruncatedSVD(n_components=1, random_state=31, n_iter=1000, tol=0.0)
 data = np.matrix(dfr.values).T
-#print data
+
 svd.fit(data)
 #print svd.components_
 #print
-#print svd.transform(data)
+#print dfr.columns
+ws = svd.transform(data)
+#print ws
+sum_w = ws[0][0] + ws[1][0] + ws[2][0]
+
+
+market_dfr = pd.DataFrame(np.matrix(svd.components_).T, index=dfr.index, columns=['market']) / sum_w
+market_dfr.to_csv('./tmp/index_svd.csv')
+
+
+#print ws[0][0] / sum_w, ws[1][0] / sum_w ,ws[2][0] / sum_w
+
+
+mr = market_dfr['market'].values
+
+
+x = mr
+x = sm.add_constant(x)
+
+
+for col in dfr.columns:
+	y = dfr[col].values
+	result = sm.OLS(y, x).fit()
+	#print result.summary()
+	print col, result.params[0] , result.params[1]
+
+
 
 
 
