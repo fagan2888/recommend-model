cd9ec5e2b9673f71068be8402c4c121f6064e23d
yjiaoneal <yjiaoneal@163.com>
2016-08-04 11:36:24 +0800

    fix bug

--- pure_alpha/shell/regression.py
+++ pure_alpha/shell/regression.py
@@ -1,14 +1,14 @@
 #coding=utf8
 
 
-
 import scipy
 import pandas as pd
+import numpy as np
 import itertools
 import time
 from sklearn import datasets, linear_model
 import datetime
-
+import statsmodels.api as sm
 
 
 def stepwise_regression():
@@ -22,32 +22,76 @@ def max_sq_r(fund_dfr, factor_dfr):
 
 	factors = list(factor_dfr.columns.values)
 
-	#for i in range(1, len(factors)):
-	for cols in list(itertools.combinations(factors, 6)):
-		#print cols #clf.fit(factor_dfr[list[cols]], fund_dfr)
-		#print type(factor_dfr[list(cols)])
-		#print fund_dfr
-		print datetime.datetime.now().microsecond
-		reg   = clf.fit(factor_dfr[list(cols)], fund_dfr.values)
-		score = reg.score(factor_dfr[list(cols)], fund_dfr.values)
-		print datetime.datetime.now().microsecond
-		print 
 
+	vs = []
+	#for i in range(1, len(factors)):
+	for cols in list(itertools.combinations(factors, 4)):
+		#reg   = clf.fit(factor_dfr[list(cols)], fund_dfr.values)
+		#score = reg.score(factor_dfr[list(cols)], fund_dfr.values)
+		#print  score, reg.intercept_
+
+		X = factor_dfr[list(cols)].values
+		y = fund_dfr.values
+
+		X = sm.add_constant(X)
+		model = sm.OLS(y, X)
+		results = model.fit()
+		alpha   = results.params[0]
+		rsq = results.rsquared_adj
+		vs.append([alpha, rsq, cols])
+
+		#print results.summary()
+		#print results.use_t
+		#print results.params
+		#print results.bse
+		#print(results.pvalues)
+
+
+		'''
+		pvalues = results.pvalues
+		sig_factor   = []		
+		for i in range(1, len(pvalues)):
+			if pvalues[i] <= 0.05:
+				sig_factor.append(cols[i-1])
+
+		#print sig_factor			
+		X = factor_dfr[sig_factor].values
+		X = sm.add_constant(X)
+		model = sm.OLS(y, X)
+		results = model.fit()
+		'''
+		#print results.rsquared_adj
+		#print 
+
+
+	df = pd.DataFrame(vs, columns = ['alpha','rsq', 'factor'])				
+	return df
 
 
 if __name__ == '__main__':
 
 
-	factor_dfr = pd.read_excel('./data/factors.xlsx', index_col='Date')
-	factor_dfr = factor_dfr.fillna(0.0)
-	#print factor_dfr
-	fund_dfr = pd.read_excel('./data/ls.xlsx', index_col='Date')
-	fund_dfr = fund_dfr.fillna(0.0)
+	factor_dfr = pd.read_csv('./data/factor.csv', index_col='date', parse_dates=['date']).pct_change().fillna(0.0)
+	fund_dfr = pd.read_csv('./data/fund.csv', index_col='date', parse_dates=['date']).pct_change().fillna(0.0)
 
 	cols     = fund_dfr.columns
-	fund_dfr = fund_dfr[cols[0]]
-	max_sq_r(fund_dfr, factor_dfr)
+	for col in cols:
 
+		tmp_fund_dfr = fund_dfr[col]
+		df = max_sq_r(tmp_fund_dfr, factor_dfr)
+
+		df = df.sort(['rsq'])
+		print df
+		#df.to_csv(str(col) + '.csv')
+		#alphas = df['alpha'].values
+		#print alphas
+		#print df
+		#index = df.index
+		#last_index = index[-1]		
+		#print df.loc[last_index, : 'alpha'].values
+		#print df.loc[last_index, , 'rsq'].values
+		#print col, df.loc[last_index, 'alpha'] * 0.25 + 1.0 / df.loc[last_index, 'rsq'] * 0.25 + np.mean(alphas) / np.std(alphas) * 0.5
+		#print col, np.mean(alphas) / np.std(alphas) * 0.5
 	#max_sq_r(None, None)
 	#print 'hehe'
 
--- pure_alpha/shell/svd.py
+++ pure_alpha/shell/svd.py
@@ -10,10 +10,12 @@ import numpy as np
 import statsmodels.api as sm
 
 
-df = pd.read_csv('./data/clean_funds.csv', index_col = 'date', parse_dates= ['date'])
-
+df = pd.read_csv('./data/fund.csv', index_col = 'date', parse_dates= ['date'])
 
 dfr = df.pct_change().fillna(0.0)
+
+
+#print dfr.index
 #dfr = dfr[['000905.SH','000300.SH','399006.SZ']]
 
 
@@ -21,40 +23,37 @@ dfr = df.pct_change().fillna(0.0)
 #print X
 
 
-svd = TruncatedSVD(n_components=1, random_state=31, n_iter=1000, tol=0.0)
+svd = TruncatedSVD(n_components=4, random_state=10, n_iter=1000, tol=0.0)
 data = np.matrix(dfr.values).T
 
 svd.fit(data)
-#print svd.components_
+
 #print
 #print dfr.columns
-ws = svd.transform(data)
+#ws = svd.transform(data)
 #print ws
-sum_w = ws[0][0] + ws[1][0] + ws[2][0]
+#sum_w = ws[0][0] + ws[1][0] + ws[2][0]
 
 
-market_dfr = pd.DataFrame(np.matrix(svd.components_).T, index=dfr.index, columns=['market']) / sum_w
-market_dfr.to_csv('./tmp/index_svd.csv')
+#market_dfr = pd.DataFrame(np.matrix(svd.components_).T, index=dfr.index, columns=['market']) / sum_w
+#market_dfr.to_csv('./tmp/index_svd.csv')
 
 
 #print ws[0][0] / sum_w, ws[1][0] / sum_w ,ws[2][0] / sum_w
 
 
-mr = market_dfr['market'].values
+#mr = market_dfr['market'].values
 
 
-x = mr
+x = np.matrix(svd.components_).T
 x = sm.add_constant(x)
 
-
+print '=========================================================='
 for col in dfr.columns:
 	y = dfr[col].values
 	result = sm.OLS(y, x).fit()
 	#print result.summary()
-	print col, result.params[0] , result.params[1]
-
-
-
+	print col, result.params, result.rsquared_adj
 
 
 
