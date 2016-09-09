8e4d0a9ba0b12030dd067655c886089fc9a8c9e8
jiaoyangneal <yjiaoneal@163.com>
2016-09-09 11:27:54 +0800

    add stock price code

---
+++ barra/db/stock_price.py
@@ -0,0 +1,55 @@
+#coding=utf8
+
+
+import MySQLdb
+import pandas as pd
+import numpy  as np
+
+
+db_params = {
+            "host": "101.201.81.170",
+            "port": 3306,
+            "user": "wind",
+            "passwd": "wind",
+            "db":"caihui_test",
+            "charset": "utf8"
+        }
+
+
+#所有股票的收盘价
+def all_stock_price():
+
+    conn = MySQLdb.connect(**db_params)
+    cur  = conn.cursor()
+
+    sql = 'select SECODE, SYMBOL from TQ_SK_BASICINFO'
+    cur.execute(sql)
+
+    secode_symbol_dict = {}
+    for record in cur.fetchall():
+        secode = record[0]
+        symbol = record[1]
+        if symbol.find('00') == 0 or symbol.find('60') == 0 or symbol.find('30') == 0:
+            #print symbol , secode
+            secode_symbol_dict[secode] = symbol
+    cur.close()
+
+    #print secode_symbol_dict
+    secodes = secode_symbol_dict.keys()
+
+    dfs = []
+    for secode in secodes:
+        sql = 'select TRADEDATE, TCLOSE from TQ_QT_SKDAILYPRICE where SECODE = %s order by TRADEDATE asc' % secode
+        df = pd.read_sql(sql, conn, index_col = 'TRADEDATE', parse_dates = ['TRADEDATE'])
+        df.index.name = 'date'
+        df.columns    = [secode_symbol_dict[secode]]
+        df.replace(0.00, np.nan, inplace = True)
+        dfs.append(df)
+        print secode_symbol_dict[secode], 'done'
+    stock_df = pd.concat(dfs, axis = 1)
+
+    stock_df.to_csv('stock_price.csv')
+
+
+if __name__ == '__main__':
+    all_stock_price()
