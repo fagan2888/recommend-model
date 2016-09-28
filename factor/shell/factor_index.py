bf6f63cf3187c56ea1d681b6f0258534b650860a
jiaoyangneal <yjiaoneal@163.com>
2016-09-28 15:31:25 +0800

    beta index

--- factor/shell/factor_index.py
+++ factor/shell/factor_index.py
@@ -2,21 +2,46 @@
 
 
 import pandas as pd
+import numpy  as np
+import time
 
 
-def beta_index(beta_df):
+def beta_index(beta_df, stock_dfr):
 
     dates = beta_df.index
     codes = beta_df.columns
 
+    rs = []
     for d in dates:
-        betas = beta_df.loc[d]
-        print betas
+        betas         = beta_df.loc[d]
+        betas.sort()
+        l             = len(betas)
+        betas         = betas[(int)(0.1 * l) : (int)(0.9 * l)]
+        l             = len(betas)
+        low_betas     = betas[0 :(int)(0.5 * l)]
+        high_betas    = betas[(int)(0.5 * l) : -1]
+        low_betas_sum = np.sum(low_betas.values)
+        high_betas_sum= np.sum(high_betas.values)
 
-    return 1
+        low_betas_rs  = []
+        high_betas_rs = []
+        for code in low_betas.index:
+            low_betas_rs.append(stock_dfr.loc[d, code])
+        for code in high_betas.index:
+            high_betas_rs.append(stock_dfr.loc[d, code])
+        r = np.mean(low_betas_rs) - np.mean(high_betas_rs)
+        print d, r
+        rs.append(r)
+
+    df = pd.DataFrame(rs, index = dates, columns = ['beta_r'])
+    df.to_csv('./tmp/beta_index.csv')
+    #print df
+    return df
 
 
 if __name__ == '__main__':
     beta_df = pd.read_csv('./tmp/beta.csv', index_col = 'date', parse_dates = ['date'])
-    #print beta_df
-    beta_index(beta_df)
+    stock_df = pd.read_csv('./data/stock_price_adjust.csv', index_col = 'date', parse_dates = ['date'])
+    stock_dfr = stock_df.pct_change().fillna(0.0)
+
+    beta_index(beta_df, stock_dfr)
