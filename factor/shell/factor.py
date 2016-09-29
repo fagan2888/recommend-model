91fe4bf34ca732c9a2861ab700c98e04dd389c58
jiaoyangneal <yjiaoneal@163.com>
2016-09-29 18:16:24 +0800

    add factor code

--- factor/shell/factor.py
+++ factor/shell/factor.py
@@ -5,20 +5,21 @@ import pandas as pd
 import string
 import statsmodels.api as sm
 import numpy as np
+import time
 
 
 #beta factor
 def beta(stock_dfr, index_dfr):
 
-
     back = 252
     dates = stock_dfr.index
 
     betas = []
-    for i in range(back, len(dates)):
+    ds    = []
+    for i in range(back, len(dates) + 1):
         tmp_stock_dfr = stock_dfr.iloc[i - back: i,]
         tmp_index_dfr = index_dfr.loc[tmp_stock_dfr.index]
-        d = dates[i]
+        d = dates[i - 1]
         beta = []
         for col in tmp_stock_dfr.columns:
             X = tmp_stock_dfr[col].values
@@ -32,13 +33,14 @@ def beta(stock_dfr, index_dfr):
             except:
                 beta.append(0.0)
         betas.append(beta)
+        ds.append(d)
         print d
 
-
-    beta_df = pd.DataFrame(betas, index = dates[252:], columns = stock_dfr.columns)
+    beta_df = pd.DataFrame(betas, index = ds, columns = stock_dfr.columns)
     #print beta_df
+    beta_df.index.name = 'date'
     beta_df.to_csv('./tmp/beta.csv')
-
+    print 'beta done'
     return beta_df
 
 
@@ -47,19 +49,21 @@ def momentum(stock_dfr):
 
     T = 504
     L = 21
+
     dfr = pd.rolling_sum(stock_dfr, L)
     cols = dfr.columns
     dates = dfr.index
 
     indexs = []
-    for j in range(0, T):
-        if j % L == 0:
-            indexs.append(j)
+    j = T - 1
+    while j >= 0:
+        indexs.append(j)
+        j = j - L
 
     momentums = []
     ds        = []
     for i in range(T, len(dates)):
-        d = dates[i]
+        d = dates[i - 1]
         tmp_dfr = dfr.iloc[ i - T : i, ]
         log_dfr = np.log(1 + tmp_dfr)
         log_dfr = log_dfr.iloc[indexs]
@@ -75,21 +79,93 @@ def momentum(stock_dfr):
     mom_df.index.name = 'date'
     mom_df.to_csv('./tmp/momentum.csv')
 
+    print 'momentum done'
     return mom_df
 
 
-def cap_size():
-    
+def cap_size(stock_market_value_df):
+    stock_market_value_df = np.log(stock_market_value_df)
+    stock_market_value_df.index.name = 'date'
+    stock_market_value_df.to_csv('./tmp/market_value.csv')
+    print 'stock cap size done'
+    return stock_market_value_df
+
+
+def dastd(stock_dfr):
+    back = 252
+    stock_dastd = pd.rolling_std(stock_dfr, back)
+    stock_dastd.index.name = 'date'
+    stock_dastd.to_csv('./tmp/stock_dastd.csv')
+    print 'stock dastd done'
+    return stock_dastd
+
+
+def cmra(stock_dfr):
+
+    T = 12
+    L = 21
+
+    dfr   = pd.rolling_sum(stock_dfr, L)
+    cols  = dfr.columns
+    dates = dfr.index
+
+    indexs = []
+    j = T * L - 1
+    while j >= 0:
+        indexs.append(j)
+        j = j - L
+
+    cmras     = []
+    ds        = []
+    for i in range(T * L, len(dates)):
+        d = dates[i - 1]
+        log_dfr = np.log(1 + tmp_dfr)
+        log_dfr = log_dfr.iloc[indexs]
+
+        zts = []
+        for j in range(1, T + 1):
+            zt      = np.sum(log_dfr.iloc[0 : j,])
+            zts.append(zt)
+
+        print d
+        momentums.append(moment)
+        ds.append(d)
+
+    mom_df = pd.DataFrame(momentums, index = ds, columns = cols)
+    mom_df.index.name = 'date'
+    mom_df.to_csv('./tmp/momentum.csv')
+
+    print 'momentum done'
+    return mom_df
+
     return 1
+
+
+def egrlf(stock_dfr):
+    return 1
+
+
+def bp(stock_bp):
+    return 1
+
 
 if __name__ == '__main__':
 
+
     stock_df = pd.read_csv('./data/stock_price_adjust.csv', index_col = 'date', parse_dates = ['date'])
-    #stock_df = pd.read_csv('./data/index_price.csv', index_col = 'date', parse_dates = ['date'])
+    stock_market_value_df = pd.read_csv('./data/stock_market_value.csv', index_col = 'date', parse_dates = ['date'])
+    stock_bp_df = pd.read_csv('./data/stock_bp.csv', index_col = 'date', parse_dates = ['date'])
     index_df = pd.read_csv('./data/index_price.csv', index_col = 'date', parse_dates = ['date'])
+
     index_df = index_df[['000300']]
     stock_dfr = stock_df.pct_change().fillna(0.0)
     index_dfr = index_df.pct_change().fillna(0.0)
+    stock_market_value_df = stock_market_value_df.fillna(method = 'pad')
+    stock_market_value_df = stock_market_value_df[stock_df.columns]
+    stock_bp_df = stock_bp_df[stock_df.columns]
 
-    beta(stock_dfr, index_dfr)
-    momentum(stock_dfr)
+    #beta(stock_dfr, index_dfr)
+    #momentum(stock_dfr)
+    #cap_size(stock_market_value_df)
+    #dastd(stock_dfr)
+    #cmra(stock_dfr)
--- factor/shell/factor_index.py
+++ factor/shell/factor_index.py
@@ -6,34 +6,44 @@ import numpy  as np
 import time
 
 
-def index(beta_df, stock_dfr):
+def index(factor_df, stock_dfr):
 
-    dates = beta_df.index
-    codes = beta_df.columns
+    factor_df = factor_df[stock_dfr.columns]
+
+    factor_dates = factor_df.index.values
+    stock_dates  = stock_dfr.index.values
+    dates        = list(set(factor_dates) & set(stock_dates))
+    dates.sort()
+    codes = factor_df.columns
 
     rs = []
-    for d in dates:
-        betas         = beta_df.loc[d]
-        betas.sort()
-        l             = len(betas)
-        betas         = betas[(int)(0.1 * l) : (int)(0.9 * l)]
-        l             = len(betas)
-        low_betas     = betas[0 :(int)(0.2 * l)]
-        high_betas    = betas[(int)(0.2 * l) : -1]
-        low_betas_sum = np.sum(low_betas.values)
-        high_betas_sum= np.sum(high_betas.values)
+    ds = []
+    for i in range(0, len(dates) - 1):
+        d      = dates[i]
+        next_d = dates[i + 1]
 
-        low_betas_rs  = []
-        high_betas_rs = []
-        for code in low_betas.index:
-            low_betas_rs.append(stock_dfr.loc[d, code])
-        for code in high_betas.index:
-            high_betas_rs.append(stock_dfr.loc[d, code])
-        r = (np.mean(high_betas_rs) - np.mean(low_betas_rs))
-        print d, r
+        factors         = factor_df.loc[d]
+        factors.sort()
+        l               = len(factors)
+        factors         = factors[(int)(0.1 * l) : (int)(0.9 * l)]
+        l               = len(factors)
+        low_factors     = factors[0 :(int)(0.2 * l)]
+        high_factors    = factors[(int)(0.2 * l) : -1]
+
+        low_factors_rs  = []
+        high_factors_rs = []
+        for code in low_factors.index:
+            low_factors_rs.append(stock_dfr.loc[next_d, code])
+        for code in high_factors.index:
+            high_factors_rs.append(stock_dfr.loc[next_d, code])
+        r = (np.mean(high_factors_rs) - np.mean(low_factors_rs))
+
+        print next_d, r
         rs.append(r)
+        ds.append(next_d)
 
-    df = pd.DataFrame(rs, index = dates, columns = ['r'])
+    df = pd.DataFrame(rs, index = ds, columns = ['r'])
+    df.index.name = 'date'
     #print df
     return df
 
@@ -42,13 +52,21 @@ if __name__ == '__main__':
 
     beta_df = pd.read_csv('./tmp/beta.csv', index_col = 'date', parse_dates = ['date'])
     momentum_df = pd.read_csv('./tmp/momentum.csv', index_col = 'date', parse_dates = ['date'])
+    market_value_df = pd.read_csv('./tmp/market_value.csv', index_col = 'date', parse_dates = ['date'])
+    dastd_df        = pd.read_csv('./tmp/stock_dastd.csv', index_col = 'date', parse_dates = ['date'])
 
     stock_df = pd.read_csv('./data/stock_price_adjust.csv', index_col = 'date', parse_dates = ['date'])
     stock_dfr = stock_df.pct_change().fillna(0.0)
 
-    #df = beta_index(beta_df, stock_dfr)
+
+    #df = index(beta_df, stock_dfr)
     #df.to_csv('./tmp/beta_index.csv')
 
-    df = index(momentum_df, stock_dfr)
-    df.to_csv('./tmp/momentum_index.csv')
-    print df
+    #df = index(momentum_df, stock_dfr)
+    #df.to_csv('./tmp/momentum_index.csv')
+
+    #df = index(market_value_df, stock_dfr)
+    #df.to_csv('./tmp/market_value_index.csv')
+
+    df = index(dastd_df, stock_dfr)
+    df.to_csv('./tmp/dastd_index.csv')
