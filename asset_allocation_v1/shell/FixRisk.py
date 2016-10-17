a4d6786ba19283a3a643b88448741d5505b88210
Likun Liu <liulikun@gmail.com>
2016-10-17 17:45:50 +0800

    add FixRisk & WeekFund2DayNav.py

---
+++ asset_allocation_v1/shell/FixRisk.py
@@ -0,0 +1,152 @@
+#coding=utf8
+
+
+
+import pandas as pd
+import numpy  as np
+import sys
+sys.path.append('shell')
+import datetime
+import AllocationData
+
+
+
+def intervalreturn(df, interval):
+
+    dates = df.index
+
+    rs = []
+    ds = []
+    for i in range(interval, len(dates)):
+        d = dates[i]
+        r = 1.0 * df.iloc[i, 0] / df.iloc[i - interval, 0] - 1
+        rs.append(r)
+        ds.append(d)
+
+    df = pd.DataFrame(rs, index=ds, columns=['nav'])
+    return df
+
+
+
+def periodstdmean(df, period):
+
+    dates = df.index
+
+    meanstd = []
+    ds      = []
+    for i in range(period, len(dates)):
+        d  = dates[i]
+        rs = df.iloc[i - period : i, 0]
+
+        meanstd.append([np.std(rs), np.mean(rs)])
+        ds.append(d)
+
+    df = pd.DataFrame(meanstd, index=ds, columns=['std','mean'])
+
+    return df
+
+
+
+def fixrisk(allocationdata):
+
+    
+    interval     = 20
+    short_period = 20
+    long_period  = 252
+
+
+    alldf = allocationdata.label_asset_df
+
+    position_datas = []
+    position_dates = []
+
+    for code in alldf.columns:
+
+        df = alldf[[code]]
+
+
+        ma5_df  = pd.rolling_mean(df, 5)
+        ma5_dfr = ma5_df.pct_change().fillna(0.0)
+        ma10_df  = pd.rolling_mean(df, 10)
+        ma10_dfr = ma10_df.pct_change().fillna(0.0)
+        ma20_df  = pd.rolling_mean(df, 20)
+        ma20_dfr = ma20_df.pct_change().fillna(0.0)
+        ma60_df  = pd.rolling_mean(df, 60)
+        ma60_dfr = ma20_df.pct_change().fillna(0.0)
+
+
+        dfr = df.pct_change().fillna(0.0)
+
+        interval_df = intervalreturn(df, interval)
+        periodstdmean_df = periodstdmean(interval_df, short_period)
+
+
+        dates = periodstdmean_df.index
+
+        ps    = [0]
+        pds   = [dates[long_period]]
+
+
+        for i in range(long_period, len(dates) - 2):
+
+            d = dates[i]
+
+            risk    = periodstdmean_df.iloc[i, 0]
+            r       = periodstdmean_df.iloc[i, 1]
+
+            risks   = periodstdmean_df.iloc[i - long_period : i + 1 , 0]
+            rs      = periodstdmean_df.iloc[i - long_period : i + 1 , 1]
+
+            rerisks  = risks
+            rers     = rs
+
+            riskstd     = np.std(rerisks)
+            riskmean    = np.mean(rerisks)
+
+            rstd        = np.std(rers)
+            rmean       = np.mean(rers)
+
+            if ma5_dfr.loc[d, code] > 0 and ma10_dfr.loc[d, code] > 0 and ma20_dfr.loc[d, code] > 0:
+                position = riskmean / risk
+                if position >= 1.0:
+                    position = 1.0
+            elif risk >= riskmean + 2 * riskstd and r < rmean - 1 * rstd:
+                position = riskmean / risk
+            elif risk >= riskmean + 2 * riskstd and r > rmean + 1 * rstd:
+                position = 0.0
+                #position = riskmean / risk
+            elif risk <= riskmean:
+                position = 1.0
+            else:
+                position = riskmean / risk
+
+            p = ps[-1]
+            if position == 0.0:
+                position = 0
+            elif (not (position <= p * 0.5 or position >= p * 2.0)) and (not (position <= p - 0.2 or position >= p + 0.2)):
+                position = p
+
+            #if position < 0.1:
+            #    position = 0.0
+
+            ps.append(position)
+            pds.append(dates[i + 1])
+
+        position_datas.append(ps)
+        position_dates = pds
+
+    pdf = pd.DataFrame(np.matrix(position_datas).T, index = position_dates, columns = alldf.columns)
+    pdf.index.name = 'date'
+    pdf.to_csv('./tmp/equalriskassetratio.csv')
+
+    allocationdata.equal_risk_asset_ratio_df = pdf
+
+
+if __name__ == '__main__':
+
+
+    df = pd.read_csv('./tmp/labelasset.csv', index_col = 'date', parse_dates = ['date'])
+    allocationdata = AllocationData.allocationdata()
+    allocationdata.label_asset_df = df
+    fixrisk(allocationdata)
+
---
+++ asset_allocation_v1/shell/WeekFund2DayNav.py
@@ -0,0 +1,150 @@
+#coding=utf8
+
+
+import pandas as pd
+import numpy  as np
+import os
+import sys
+sys.path.append('shell')
+import datetime
+import DBData
+import DFUtil
+import AllocationData
+import re
+from Const import datadir
+
+pattern = re.compile(r'\d+')
+
+def codes_r(d, dfr, codes):
+	cs = pattern.findall(codes)
+	r = 0.0
+	for code in cs:
+		r = r + dfr.loc[d, code] / len(cs)
+	return r
+
+
+def week2day(allocationdata):
+
+
+	stock_df = pd.read_csv(os.path.join(datadir, 'stock_fund.csv'), index_col = 'date', parse_dates = ['date'])
+	bond_df  = pd.read_csv(os.path.join(datadir, 'bond_fund.csv'),  index_col = 'date', parse_dates = ['date'])
+	money_df = pd.read_csv(os.path.join(datadir, 'money_fund.csv'), index_col = 'date', parse_dates = ['date'])
+
+
+	#print stock_df
+	#print bond_df
+	#print money_df
+	#print other_df
+
+
+	df = pd.concat([stock_df, bond_df, money_df], axis = 1, join_axes = [stock_df.index])
+	dates = df.index
+
+	rs      = []
+	r_dates = []
+
+	for i in range(0, len(dates)):
+
+
+		d = dates[i]
+		start_date = d
+		end_date   = datetime.datetime.now()
+		if i < len(dates) - 1:
+			end_date = dates[i + 1]	
+
+		stock_value_df   = DBData.stock_day_fund_value(start_date, end_date)
+		bond_value_df    = DBData.bond_day_fund_value(start_date, end_date)
+		money_value_df   = DBData.money_day_fund_value(start_date, end_date)
+                print start_date, end_date
+		index_value_df   = DBData.index_day_value(start_date, end_date)
+
+                if 'GLNC' not in index_value_df.columns:
+                        index_value_df['GLNC'] = 287.8400
+
+
+		stock_value_dfr  = stock_value_df.pct_change().fillna(0.0).iloc[1:,:]
+		bond_value_dfr   = bond_value_df.pct_change().fillna(0.0).iloc[1:,:]
+		money_value_dfr  = money_value_df.pct_change().fillna(0.0).iloc[1:,:]
+		index_value_dfr  = index_value_df.pct_change().fillna(0.0).iloc[1:,:]
+
+
+		large_code       = stock_df.loc[d, 'largecap']	
+		small_code       = stock_df.loc[d, 'smallcap']	
+		rise_code        = stock_df.loc[d, 'rise']	
+		oscillation_code = stock_df.loc[d, 'oscillation']	
+		decline_code     = stock_df.loc[d, 'decline']	
+		growth_code      = stock_df.loc[d, 'growth']	
+		value_code       = stock_df.loc[d, 'value']	
+
+		ratebond_code    = bond_df.loc[d,'ratebond']
+		creditbond_code  = bond_df.loc[d,'creditbond']
+		convertiblebond_code = bond_df.loc[d,'convertiblebond']
+
+		#money_code       = money_df.loc[d, 'money']
+		money_code       = "%06d" % money_df.loc[d, 'money']
+
+
+		sp500_code       = 'SP500.SPI'
+		gold_code        = 'GLNC'
+		hs_code          = 'HSCI.HI'
+
+
+		for tmp_d in stock_value_dfr.index:
+
+			r = []
+
+			r.append(codes_r(tmp_d, stock_value_dfr, large_code))
+			r.append(codes_r(tmp_d, stock_value_dfr, small_code))
+			r.append(codes_r(tmp_d, stock_value_dfr, rise_code))
+			r.append(codes_r(tmp_d, stock_value_dfr, oscillation_code))
+			r.append(codes_r(tmp_d, stock_value_dfr, decline_code))
+			r.append(codes_r(tmp_d, stock_value_dfr, growth_code))
+			r.append(codes_r(tmp_d, stock_value_dfr, value_code))
+
+			r.append(codes_r(tmp_d, bond_value_dfr, ratebond_code))
+			r.append(codes_r(tmp_d, bond_value_dfr, creditbond_code))
+			r.append(codes_r(tmp_d, bond_value_dfr, convertiblebond_code))
+
+			r.append(money_value_dfr.loc[tmp_d, money_code])
+
+			r.append(index_value_dfr.loc[tmp_d, sp500_code])
+			r.append(index_value_dfr.loc[tmp_d, gold_code])
+			r.append(index_value_dfr.loc[tmp_d, hs_code])
+
+			rs.append(r)
+
+			tmp_d = datetime.datetime.strftime(tmp_d, '%Y-%m-%d')
+			r_dates.append(datetime.datetime.strptime(tmp_d, '%Y-%m-%d'))
+			print tmp_d ,r
+
+
+	df = pd.DataFrame(rs, index = r_dates, columns = ['largecap','smallcap','rise', 'oscillation', 'decline', 'growth','value', 'ratebond', 'creditbond','convertiblebond','money', 'SP500.SPI', 'GLNC','HSCI.HI'])
+	df.index.name = 'date'
+
+
+	dfr = df
+	values = []
+	for col in dfr.columns:
+		rs = dfr[col].values
+		vs = [1]
+		for i in range(1, len(rs)):
+			r = rs[i]
+			v = vs[-1] * ( 1.0 + r )	
+			vs.append(v)
+		values.append(vs)
+	
+	alldf = pd.DataFrame(np.matrix(values).T, index = dfr.index, columns = dfr.columns)
+
+	allocationdata.label_asset_df = alldf	
+	alldf.to_csv(os.path.join(datadir, 'labelasset.csv'))
+
+	week_df = alldf.resample('W-FRI').last()
+	week_df = week_df.fillna(method = 'pad')
+	week_df.to_csv(os.path.join(datadir, 'labelassetweek.csv'))
+
+		#print start_date, d, end_date			
+	#print df
+
+
+if __name__ == '__main__':
+	print
