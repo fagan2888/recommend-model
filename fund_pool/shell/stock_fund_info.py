c3073ccd450d8ba8cbe493f6f624fd85687bf058
jiaoyangneal <yjiaoneal@163.com>
2016-10-20 14:24:22 +0800

    add code

--- fund_pool/shell/stock_fund_info.py
+++ fund_pool/shell/stock_fund_info.py
@@ -26,6 +26,15 @@ db_base = {
 }
 '''
 
+db_base = {
+    "host": "101.201.81.170",
+    "port": 3306,
+    "user": "wind",
+    "passwd": "wind",
+    "db":"caihui_test",
+    "charset": "utf8"
+}
+
 
 if __name__ == '__main__':
 
@@ -36,18 +45,25 @@ if __name__ == '__main__':
     for code in df.index:
         codes.append('%06d' % code)
 
+
     imploded_sids = ','.join([repr(sid.encode('utf8')) for sid in codes])
+    sql = 'select FDSNAME, FDNATURE, INVESTSTYLE, FOUNDDATE, SECURITYID, FSYMBOL, MANAGERNAME from TQ_FD_BASICINFO where SECURITYID in (' + imploded_sids + ')'
+    fund_info_df = pd.read_sql(sql, conn, index_col = 'SECURITYID')
+    #print fund_info_df
+
+
     sql             = 'select MANAGERCODE, MANAGERNAME, NAVRTO, BENCHMARKRTO, TENUREYIELD TENUREYIELDYR, ALLRANK, ALLAVGYIELD, CLASSRANK, RANKTYPE, RTYPEAVGYIELD , SECURITYID from TQ_FD_MGPERFORMANCE where SECURITYID in (' + imploded_sids + ')'
     mgperformance_df= pd.read_sql(sql, conn, index_col = 'SECURITYID')
-
-
-    mgperformance_df.to_csv('./tmp/manager_performance.csv', encoding='utf8')
     #print mgperformance_df
 
-    #sql = 'select PSCODE, PSNAME, GENDER, BIRTH, DEGREE, TOTYEARS, URFCOUNT, AVGTENURE, TOTCOUNT from TQ_FD_MANAGERSTA'
-    #managersta_df = pd.read_sql(sql, conn)
+    #mgperformance_df.to_csv('./tmp/manager_performance.csv', encoding='utf8')
+    #print mgperformance_df
+
+    sql = 'select PSCODE, PSNAME, GENDER, BIRTH, DEGREE, TOTYEARS, CURFCOUNT, AVGTENURE, TOTCOUNT from TQ_FD_MANAGERSTA'
+    managersta_df = pd.read_sql(sql, conn)
     #print managersta_df
 
+    sql = ''
     #mgperformance_df= mgperformance_df.loc[fund_info_df.index]
 
     #df = pd.concat([fund_info_df, mgperformance_df])
