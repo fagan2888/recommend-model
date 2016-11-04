930ec04d9b95574cb6a122ead1bba398d7de9be4
jiaoyangneal <yjiaoneal@163.com>
2016-11-04 15:33:51 +0800

    add code

--- factor/shell/fund_style.py
+++ factor/shell/fund_style.py
@@ -8,10 +8,15 @@ import datetime
 
 if __name__ == '__main__':
 
-    stock_df              = pd.read_csv('./data/stock_price_adjust.csv', index_col = 'date', parse_dates = ['date'])
+
+    dates = pd.date_range('2014-01-01', '2016-11-04')
+    #print dates
+    #stock_df              = pd.read_csv('./data/stock_price_adjust.csv', index_col = 'date', parse_dates = ['date'])
     stock_market_value_df = pd.read_csv('./data/stock_market_value.csv', index_col = 'date', parse_dates = ['date'])
+    stock_market_value_df = stock_market_value_df.reindex(dates).fillna(method='pad')
     stock_pe_df           = pd.read_csv('./data/stock_pe.csv', index_col = 'date', parse_dates = ['date'])
-    all_fund_df           = pd.read_csv('./data/all_stock_fund.csv')
+    stock_pe_df           = stock_pe_df.reindex(dates).fillna(method='pad')
+    all_fund_df           = pd.read_csv('./data/all_stock_fund.csv', index_col = 'SECODE')['FSYMBOL']
     stock_code_df         = pd.read_csv('./data/stock_code.csv', index_col = ['SECODE'])['SYMBOL']
     fund_position_df      = pd.read_csv('./data/tq_fd_skdetail', index_col = ['ENDDATE', 'SECODE', 'SKCODE'], parse_dates = ['ENDDATE'])['ACCSTKRTO']
     #fund_position_df      = pd.read_csv('./data/tq_fd_skdetail', index_col = ['ENDDATE'], parse_dates = ['ENDDATE'])
@@ -27,28 +32,64 @@ if __name__ == '__main__':
         symbol = stock_code_df.loc[secode]
         stock_code_dict[secode] = symbol
 
+    #print all_fund_df
     fund_code_dict = {}
-    print all_fund_df
-    #for secode in all_fund_df.index:
-    #    print all_fund_df.loc[secode]
+    for secode in all_fund_df.index:
+        fcode = all_fund_df.loc[secode]
+        fund_code_dict[secode] = fcode
+    #print fund_code_dict
     #print   stock_code_dict
 
     #print fund_position_df.index.get_level_values(0)
     #print stock_code_dict.keys()
-    for date in fund_position_df.index.get_level_values(0):
+    secodes = []
+    fpes = []
+    fsizes = []
+    fsymbols = []
+    dates = fund_position_df.index.get_level_values(0).unique().values[0:11]
+    #print dates
+    for date in dates:
+        print date
         secode_skcode_ratio_df = fund_position_df.loc[date]
-        for secode in secode_skcode_ratio_df.index.get_level_values(0):
+        #dates.append(date)
+        secodes = secode_skcode_ratio_df.index.get_level_values(0).unique()
+        symbols = []
+        for secode in secodes:
+            #print secode, fund_code_dict[secode]
+            symbols.append(fund_code_dict[secode])
+        fsymbols = symbols
+        fpe_record = []
+        fsize_record = []
+        for secode in secodes:
             skcode_ratio_df = secode_skcode_ratio_df.loc[secode]
+            fpe = 0
+            fsize = 0
             for skcode in skcode_ratio_df.index:
+                if not stock_code_dict.has_key(skcode):
+                    continue
                 code = '%06d'  % stock_code_dict[skcode]
                 ratio  = skcode_ratio_df[skcode] / 100.0
                 size = stock_market_value_df.loc[date, code]
                 pe = stock_pe_df.loc[date, code]
-                print date, secode, code, ratio, size, pe
+                fpe = fpe + ratio * pe
+                fsize = fsize + ratio * size
+            fpe_record.append(fpe)
+            fsize_record.append(fsize)
+        fpes.append(fpe_record)
+        fsizes.append(fsize_record)
+        #print date, fpe_record, fsize_record
+                #print date, secode, code, ratio, size, pe
 
         #print secode_skcode_ratio
         #for secode in
         #print date, secode, skcode, ratio
+
+
+    fsize_df = pd.DataFrame(fsizes, index = dates, columns = fsymbols)
+    fpe_df = pd.DataFrame(fpes, index = dates, columns = fsymbols)
+    fsize_df.to_csv('fsize.csv')
+    fpe_df.to_csv('fpe.csv')
+
 
     '''
     print skcode_ratio
---
+++ factor/shell/size_pe_test.py
@@ -0,0 +1,23 @@
+#coding=utf8
+
+
+import pandas as pd
+
+
+if __name__ == '__main__':
+
+    '''
+    df = pd.read_csv('./data/fsize.csv', index_col = 'date')
+    dates = df.index
+    d = dates[-1]
+    sizes = df.loc[d].dropna()
+    sizes.sort()
+    print sizes
+    '''
+
+    df = pd.read_csv('./data/fpe.csv', index_col = 'date')
+    dates = df.index
+    d = dates[-1]
+    pes = df.loc[d].dropna()
+    pes.sort()
+    print pes
