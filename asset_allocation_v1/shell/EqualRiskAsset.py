916bc056599db60ff69c00b4ed62a0ba73ae789f
Likun Liu <liulikun@gmail.com>
2016-11-18 17:34:29 +0800

    timing reshape ok

--- asset_allocation_v1/shell/EqualRiskAsset.py
+++ asset_allocation_v1/shell/EqualRiskAsset.py
@@ -7,9 +7,14 @@ import time
 from datetime import datetime
 import os
 import sys
+import click
+import logging
 sys.path.append('shell')
 
+
 from Const import datapath
+
+logger = logging.getLogger(__name__)
 
 
 def equalriskasset():
@@ -49,64 +54,67 @@ def equalriskasset():
     result_dates = []
     result_datas  = []
 
-    for i in range(0, len(dates)):
+    ii = range(0, len(dates))
+    with click.progressbar(length=len(ii), label='update nav for reshaped assets') as bar:
+        for i in ii:
+            bar.update(1)
 
-
-        d = dates[i]
-        for asset in assetlabels:
-
-            cvs = asset_combination[asset]
-            last_cvs = cvs[-1]
-            current_cvs = [last_cvs[0] * (1 + dfr.loc[d, asset]), last_cvs[1] * (1 + rf)]
-            #current_cvs = [last_cvs[0] * (1 + dfr.loc[d, asset]), last_cvs[1]]
-            cvs.append(current_cvs)
-            vs = asset_values[asset]
-            if current_cvs[0] + current_cvs[1] == 0:
-                continue
-            else:
-                vs.append(current_cvs[0] + current_cvs[1])
-
-
-            '''
-            vs = asset_values[asset]
-            last_v = vs[-1]
-            current_v = last_v + last_v * dfr.loc[d, asset] * asset_ratio[asset]
-            vs.append(current_v)
-            '''
-
-
-        if d in ratio_dates:
+            d = dates[i]
             for asset in assetlabels:
-                asset_ratio[asset] = ratio_df.loc[d, asset]
-                vs = asset_values[asset]
+
                 cvs = asset_combination[asset]
-                cvs.append( [vs[-1] * asset_ratio[asset], vs[-1] * (1 - asset_ratio[asset])] )
+                last_cvs = cvs[-1]
+                current_cvs = [last_cvs[0] * (1 + dfr.loc[d, asset]), last_cvs[1] * (1 + rf)]
+                #current_cvs = [last_cvs[0] * (1 + dfr.loc[d, asset]), last_cvs[1]]
+                cvs.append(current_cvs)
+                vs = asset_values[asset]
+                if current_cvs[0] + current_cvs[1] == 0:
+                    continue
+                else:
+                    vs.append(current_cvs[0] + current_cvs[1])
 
 
-        #asset_vs = [asset_values['largecap'][-1], asset_values['smallcap'][-1], asset_values['rise'][-1],
-        #            asset_values['oscillation'][-1], asset_values['decline'][-1], asset_values['growth'][-1], \
-        #            asset_values['value'][-1], asset_values['convertiblebond'][-1], asset_values['SP500.SPI'][-1], \
-        #            asset_values['GLNC'][-1], asset_values['HSCI.HI'][-1]]
-        #result_datas.append(asset_vs)
-        #result_dates.append(d)
-
-        asset_vs = [asset_values['largecap'][-1], asset_values['smallcap'][-1], asset_values['rise'][-1],
-                    asset_values['oscillation'][-1], asset_values['decline'][-1], asset_values['growth'][-1], \
-                    asset_values['value'][-1], asset_values['SP500.SPI'][-1], \
-                    asset_values['GLNC'][-1], asset_values['HSCI.HI'][-1]]
-        result_datas.append(asset_vs)
-        result_dates.append(d)
+                '''
+                vs = asset_values[asset]
+                last_v = vs[-1]
+                current_v = last_v + last_v * dfr.loc[d, asset] * asset_ratio[asset]
+                vs.append(current_v)
+                '''
 
 
-        print d, asset_values['largecap'][-1], asset_values['smallcap'][-1], asset_values['rise'][-1], \
-        asset_values['oscillation'][-1], asset_values['decline'][-1], asset_values['growth'][-1], \
-            asset_values['value'][-1], asset_values['convertiblebond'][-1], asset_values['SP500.SPI'][-1], \
-            asset_values['GLNC'][-1], asset_values['HSCI.HI'][-1]
+            if d in ratio_dates:
+                for asset in assetlabels:
+                    asset_ratio[asset] = ratio_df.loc[d, asset]
+                    vs = asset_values[asset]
+                    cvs = asset_combination[asset]
+                    cvs.append( [vs[-1] * asset_ratio[asset], vs[-1] * (1 - asset_ratio[asset])] )
 
 
-        if d in ratio_dates:
-            for asset in assetlabels:
-                asset_ratio[asset] = ratio_df.loc[d, asset]
+            #asset_vs = [asset_values['largecap'][-1], asset_values['smallcap'][-1], asset_values['rise'][-1],
+            #            asset_values['oscillation'][-1], asset_values['decline'][-1], asset_values['growth'][-1], \
+            #            asset_values['value'][-1], asset_values['convertiblebond'][-1], asset_values['SP500.SPI'][-1], \
+            #            asset_values['GLNC'][-1], asset_values['HSCI.HI'][-1]]
+            #result_datas.append(asset_vs)
+            #result_dates.append(d)
+
+            asset_vs = [asset_values['largecap'][-1], asset_values['smallcap'][-1], asset_values['rise'][-1],
+                        asset_values['oscillation'][-1], asset_values['decline'][-1], asset_values['growth'][-1], \
+                        asset_values['value'][-1], asset_values['SP500.SPI'][-1], \
+                        asset_values['GLNC'][-1], asset_values['HSCI.HI'][-1]]
+            result_datas.append(asset_vs)
+            result_dates.append(d)
+
+
+            logger.info("%s %.6f %.6f %.6f %.6f %.6f %.6f %.6f %.6f %.6f %.6f %.6f", \
+                        d.strftime("%Y-%m-%d"), asset_values['largecap'][-1], asset_values['smallcap'][-1], asset_values['rise'][-1], \
+                        asset_values['oscillation'][-1], asset_values['decline'][-1], asset_values['growth'][-1], \
+                        asset_values['value'][-1], asset_values['convertiblebond'][-1], asset_values['SP500.SPI'][-1], \
+                        asset_values['GLNC'][-1], asset_values['HSCI.HI'][-1])
+
+
+            if d in ratio_dates:
+                for asset in assetlabels:
+                    asset_ratio[asset] = ratio_df.loc[d, asset]
 
 
     new_assetlabels  = ['largecap','smallcap','rise','oscillation','decline','growth','value','SP500.SPI','GLNC','HSCI.HI']
