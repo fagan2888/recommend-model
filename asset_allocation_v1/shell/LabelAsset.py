156893dddf541b5ba6b5206cf3002687401323c4
jiaoyangneal <yjiaoneal@163.com>
2016-12-02 12:15:02 +0800

    fix bug

--- asset_allocation_v1/shell/LabelAsset.py
+++ asset_allocation_v1/shell/LabelAsset.py
@@ -32,7 +32,7 @@ def label_asset_tag(label_index, lookback=52):
     # label_index = pd.DatetimeIndex(['2012-10-26', '2015-09-30', '2016-04-08', '2016-10-14'])
     #label_index = pd.DatetimeIndex(['2016-10-14'])
 
-    label_index = pd.DatetimeIndex(['2012-06-30','2012-09-30','2012-12-31','2013-03-31','2013-06-30','2013-09-30','2013-12-31','2014-03-31','2014-06-30','2014-09-30','2014-12-31', '2015-03-31','2015-06-30','2015-09-30','2015-12-31','2016-03-31','2016-06-30', '2016-09-30'])
+    #label_index = pd.DatetimeIndex(['2012-06-30','2012-09-30','2012-12-31','2013-03-31','2013-06-30','2013-09-30','2013-12-31','2014-03-31','2014-06-30','2014-09-30','2014-12-31', '2015-03-31','2015-06-30','2015-09-30','2015-12-31','2016-03-31','2016-06-30', '2016-09-30'])
 
     '''
     label_index = pd.DatetimeIndex([
@@ -216,7 +216,7 @@ def label_asset_stock_per_day(day, lookback, limit = 5):
 
     l = len(date_size_df)
     #date_size_df = date_size_df.iloc[(int)(0.1 * l) : (int)(0.6 *l)]
-    date_size_df = date_size_df.iloc[(int)(0.1 * l) : (int)(0.4 *l)]
+    date_size_df = date_size_df.iloc[(int)(0.3 * l) : (int)(0.5 *l)]
     #date_size_df = date_size_df.iloc[(int)(0.5 *l) : l]
     #date_size_df = date_size_df
     size_codes = []
