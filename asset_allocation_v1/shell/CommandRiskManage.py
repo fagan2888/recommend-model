8f2abadcfe86d8931ad64392b4583890cc2b1ca0
Likun Liu <liulikun@gmail.com>
2016-12-05 18:36:30 +0800

    rb riskmgr test

--- asset_allocation_v1/shell/CommandRiskManage.py
+++ asset_allocation_v1/shell/CommandRiskManage.py
@@ -48,7 +48,7 @@ def riskmgr(ctx):
 @click.option('--reshape/--no-reshape', default=True)
 @click.option('--markowitz/--no-markowitz', default=True)
 @click.pass_context
-def simple(ctx, datadir, startdate, enddate, label_asset, reshape, markowitz):
+def test(ctx, datadir, startdate, enddate, label_asset, reshape, markowitz):
     '''run risk management using simple strategy
     '''
     Const.datadir = datadir
@@ -73,3 +73,37 @@ def simple(ctx, datadir, startdate, enddate, label_asset, reshape, markowitz):
     df_result = risk_mgr.perform(df_nav[['sh000300']], df_pos[['sh000300']], df_timing[['sh000300']])
 
     df_result.to_csv(datapath('riskmgr_result.csv'))
+
+@riskmgr.command()
+@click.option('--datadir', '-d', type=click.Path(exists=True), default='./tmp', help=u'dir used to store tmp data')
+@click.option('--start-date', 'startdate', default='2012-07-15', help=u'start date to calc')
+@click.option('--end-date', 'enddate', help=u'end date to calc')
+@click.option('--label-asset/--no-label-asset', default=True)
+@click.option('--reshape/--no-reshape', default=True)
+@click.option('--markowitz/--no-markowitz', default=True)
+@click.pass_context
+def simple(ctx, datadir, startdate, enddate, label_asset, reshape, markowitz):
+    '''run risk management using simple strategy
+    '''
+    Const.datadir = datadir
+
+    if not enddate:
+        yesterday = (datetime.now() - timedelta(days=1)); 
+        enddate = yesterday.strftime("%Y-%m-%d")        
+
+    df_nav = pd.read_csv(datapath('equalriskasset.csv'),  index_col=['date'], parse_dates=['date'])
+
+    df_pos = pd.read_csv(datapath('port_weight.csv'),  index_col=['date'], parse_dates=['date'])
+
+    df_timing = pd.read_csv(datapath('hs_gftd.csv'),  index_col=['date'], parse_dates=['date'], usecols=['date', 'trade_types'])
+    df_timing = df_timing.rename(columns={'trade_types':'sh000300'})
+
+    print df_nav.head()
+    print df_pos.head()
+    print df_timing.head()
+
+    risk_mgr = RiskManagement.RiskManagement()
+    df_result = risk_mgr.perform(df_nav[['sh000300']], df_pos[['sh000300']], df_timing[['sh000300']])
+
+    df_result.to_csv(datapath('riskmgr_result.csv'))
+    
