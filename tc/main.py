1979b6074c051d5f6b7cff8d6ad385f2e1648404
yitao <sytlnrf@gmail.com>
2016-12-16 11:58:09 +0800

    add 000905

--- tc/main.py
+++ tc/main.py
@@ -5,7 +5,8 @@ import gftd
 import datetime
 
 if __name__ == "__main__":
-    assets = ['HSI001', '000300', 'W00003', 'CI0022']
+    # assets = ['HSI001', '000300', 'W00003', 'CI0022', '000905']
+    assets = ['000905']
     edate = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d')
     for ass in assets:
         print ass
