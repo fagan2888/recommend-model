678b3819f384030c79d8815308fa2fa5ff6a2b8a
yitao <sytlnrf@gmail.com>
2017-03-27 14:09:03 +0800

    add method to write views to database

--- asset_allocation_v1/shell/db/asset_vw_view_inc.py
+++ asset_allocation_v1/shell/db/asset_vw_view_inc.py
@@ -2,8 +2,11 @@
 
 from sqlalchemy import MetaData, Table, select, func
 import pandas as pd
+import datetime
 import logging
 import database
+import os
+import numpy as np
 from dateutil.parser import parse
 logger = logging.getLogger(__name__)
 def get_asset_newest_view(viewid):
@@ -22,3 +25,23 @@ def get_asset_newest_view(viewid):
     s = select(columns).where(t.c.vw_view_id == viewid)
     df = pd.read_sql(s, db)
     return df
+def insert_predict_pct(view_df):
+    if type(view_df) != pd.core.frame.DataFrame:
+        return (2, "view_df is not DataFrame")
+    if view_df.empty:
+        return (3, "view_df is null")
+    try:
+        db = database.connection('asset')
+        ids = view_df['ids']
+        dates = view_df['dates']
+        inc = view_df['means']
+        d = {'vw_view_id':ids, 'vw_date':dates, 'vw_inc':inc}
+        test_df = pd.DataFrame(data=d)
+        result = (0, "Insert into vw_veiw_inc success")
+        df = test_df.to_sql("vw_view_inc", db, if_exists='append', index=False)
+    except Exception, e:
+        result = (1, "Insert into vw_veiw_inc fail: " + e.message)
+    return result
+
+if __name__ == "__main__":
+    print insert_predict_pct(None)
