f902998ece17a36638c58501d12b64672080b532
jiaoyangneal <yjiaoneal@163.com>
2017-04-10 15:50:39 +0800

    add code

--- target-life/shell/export_nav.py
+++ target-life/shell/export_nav.py
@@ -12,7 +12,7 @@ db_base = {
     "port": 3306,
     "user": "root",
     "passwd": "Mofang123",
-    "db":"mofang",
+    "db":"asset_allocation",
     "charset": "utf8"
 }
 
@@ -28,15 +28,22 @@ if __name__ == '__main__':
 
 
     conn  = MySQLdb.connect(**db_base)
-    cur   = conn.cursor(MySQLdb.cursors.DictCursor)
 
     gids = []
     for i in range(0, 10):
         gid = 80041010 + i
         gids.append(gid)
 
-    gids_str =
+    gids_str = ','.join([repr(gid) for gid in gids])
 
+    sql = 'select ra_portfolio_id, ra_date, ra_nav from ra_portfolio_nav where ra_portfolio_id in (' + gids_str + ')'
+
+    df = pd.read_sql(sql, conn, index_col = ['ra_date', 'ra_portfolio_id'])
+    df = df.unstack()
+    df = df.dropna()
+    print df
+
+    df.to_csv('data/nav.csv')
 
     '''
     target_rs = []
---
+++ target-life/shell/target_life_prob_v2.py
@@ -0,0 +1,66 @@
+#coding=utf8
+
+
+import pandas as pd
+import numpy as np
+import scipy.optimize
+
+
+def obj_fun(x, ):
+    p = 1
+    for i in range(0, 10):
+       p = p * ((((1 - 1.0 * i / 10) ** 0.5) * x) + 1 )
+    return (p - 1.06) ** 2
+
+
+if __name__ == '__main__':
+
+
+    df = pd.read_csv('./data/nav.csv', index_col = ['date'], parse_dates = ['date'])
+    df = df.resample('M', how='last')
+    dfr = df.pct_change().dropna()
+    rs_dfr = dfr.mean()
+    rs = list(rs_dfr.values)
+    rs.sort()
+
+
+    target_rs = []
+    x = 0
+    res = scipy.optimize.minimize(obj_fun, x, method='SLSQP')
+    x = res.x[0]
+    for i in range(0, 10):
+        target_rs.append(((1 - 1.0 * i / 10) ** 0.5) * x)
+    target_rs.sort(reverse = True)
+
+
+    risk_levels = []
+    for i in range(0, len(target_rs)):
+        target_r = target_rs[i]
+        for j in range(0, len(rs)):
+            if (j == 0 and target_r  < rs[j]) or (target_r >= rs[j - 1] and target_r <= rs[j]):
+                #print j + 1
+                risk_levels.append(j + 1)
+                break
+
+    total = 0
+    num = 0
+    dates = dfr.index
+    for i in range(0, len(dates) - 10):
+        v = 1
+        for j in range(0, len(risk_levels)):
+            risk_level = risk_levels[j]
+            d = dates[ i + j ]
+            r = dfr.loc[d, 'risk' + str( j + 1 )]
+            #print d, risk_level, r
+            v = v * (r + 1)
+        print dates[i], v
+        total = total + 1
+        if v > 1.06:
+            num = num + 1
+
+    print total
+    print num
+    print 1.0 * num / total
+    #print rs
+    #print target_rs
+    print risk_levels
