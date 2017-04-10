f996567f5ef6c4784d3271e4dae4e8576a94fdb8
Likun Liu <liulikun@gmail.com>
2017-04-10 14:19:28 +0800

    riskmgr mindd set to 0.5

--- asset_allocation_v1/shell/RiskMgrSimple.py
+++ asset_allocation_v1/shell/RiskMgrSimple.py
@@ -26,7 +26,7 @@ def confidence(x, per):
 
 class RiskMgrSimple(object):
 
-    def __init__(self, empty=5, maxdd=-0.075, mindd=-0.07, period=252):
+    def __init__(self, empty=5, maxdd=-0.075, mindd=-0.05, period=252):
         self.maxdd = maxdd
         self.mindd = mindd
         self.empty = empty
