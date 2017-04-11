61edb489ef32b4d6a4e00ec18a0bbcccffd3ae55
jiaoyangneal <yjiaoneal@163.com>
2017-04-11 15:19:03 +0800

    add code

--- target-life/shell/target_life_prob_v2.py
+++ target-life/shell/target_life_prob_v2.py
@@ -6,11 +6,11 @@ import numpy as np
 import scipy.optimize
 
 
-def obj_fun(x, ):
+def obj_fun(x, target_r, n):
     p = 1
-    for i in range(0, 10):
+    for i in range(0, n):
        p = p * ((((1 - 1.0 * i / 10) ** 0.5) * x) + 1 )
-    return (p - 1.06) ** 2
+    return (p - target_r) ** 2
 
 
 if __name__ == '__main__':
@@ -24,6 +24,74 @@ if __name__ == '__main__':
     rs.sort()
 
 
+    months = 10
+    target_r = 1.06
+    total = 0
+    num = 0
+    dates = dfr.index
+    for i in range(0, len(dates) - months):
+
+        v = 1.0
+        tmp_target_r = target_r
+        d = dates[i]
+
+        for j in range(0, months):
+            tmp_d = dates[i + j]
+            n = months - j
+            x = 0
+            res = scipy.optimize.minimize(obj_fun, x, (tmp_target_r, n), method='SLSQP')
+            x = res.x[0]
+            risk_level = -1
+            for p in range(0, len(rs)):
+                if (p == 0 and x < rs[p]) or (x >= rs[p - 1] and x <= rs[p]):
+                    risk_level = p + 1
+                    break
+
+            #print d, tmp_d, risk_level, v
+
+            if risk_level == -1:
+                #print d, tmp_d, v, 'not arrival'
+                #break
+                risk_level = 10
+
+            r = dfr.loc[tmp_d, 'risk' + str( risk_level)]
+            v = v * (1 + r)
+            if v >= target_r:
+                break
+            else:
+                tmp_target_r =  tmp_target_r / v
+
+        if v >= target_r:
+            num = num + 1
+        total = total + 1
+
+    print total
+    print num
+    print 1.0 * num / total
+        #print d, v
+
+
+    '''
+    total = 0
+    num = 0
+    dates = dfr.index
+    for i in range(0, len(dates) - 10):
+        v = 1
+        for j in range(0, len(risk_levels)):
+            risk_level = risk_levels[j]
+            d = dates[ i + j ]
+            r = dfr.loc[d, 'risk' + str( j + 1 )]
+            #print d, risk_level, r
+            v = v * (r + 1)
+        print dates[i], v
+        total = total + 1
+        if v > 1.06:
+            num = num + 1
+
+    print total
+    print num
+    print 1.0 * num / total
+
     target_rs = []
     x = 0
     res = scipy.optimize.minimize(obj_fun, x, method='SLSQP')
@@ -33,7 +101,6 @@ if __name__ == '__main__':
     target_rs.sort(reverse = True)
 
 
-    risk_levels = []
     for i in range(0, len(target_rs)):
         target_r = target_rs[i]
         for j in range(0, len(rs)):
@@ -64,3 +131,4 @@ if __name__ == '__main__':
     #print rs
     #print target_rs
     print risk_levels
+    '''
