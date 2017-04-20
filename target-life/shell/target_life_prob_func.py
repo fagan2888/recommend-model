20bc8946eac65174d0200a06e2cd86caf4da67e9
jiaoyangneal <yjiaoneal@163.com>
2017-04-20 13:44:49 +0800

    add code

--- target-life/shell/target_life_prob_func.py
+++ target-life/shell/target_life_prob_func.py
@@ -50,6 +50,8 @@ def prob(df, target_r, risk_levels, n, interval):
     total = 0
     num = 0
     dates = dfr.index
+    #vs = {}
+    vs = []
     for i in range(0, len(dates) - n):
         v = 1
         for j in range(0, len(risk_levels)):
@@ -57,16 +59,19 @@ def prob(df, target_r, risk_levels, n, interval):
             d = dates[ i + j ]
             r = dfr.loc[d, 'risk' + str(risk_level)]
             v = v * (r + 1)
-        print dates[i], v
+        #print dates[i], v
         total = total + 1
         if v > target_r:
             num = num + 1
+        #vs[dates[i]] = v
+        vs.append(v)
 
-    print total
-    print num
-    print 1.0 * num / total
+    #print total
+    #print num
+    return 1.0 * num / total, vs
     #print rs
     #print target_rs
+
 
 if __name__ == '__main__':
 
@@ -74,15 +79,33 @@ if __name__ == '__main__':
     df = pd.read_csv('./data/nav.csv', index_col = ['date'], parse_dates = ['date'])
     df = df.resample('M', how='last')
 
-    target_r = 1.04
-    n = 10
+    #target_r = 1.045
+    n = 12
     interval = 1
 
     #risk_levels = target_life_risklevels(df, target_r, n, interval)
-    risk_levels = []
-    for i in range(0, n):
-        risk_levels.append(6)
+    target_rs = [1.06, 1.08, 1.10, 1.12]
+    for i in range(0, 10):
+        risk_levels = []
+        for j in range(0, n):
+            risk_levels.append(i + 1)
+        for target_r in target_rs:
+            p, vs = prob(df, target_r, risk_levels, n , interval)
+            v_sum = 0
+            ex_profit = 0
+            nex_profit = 0
+            for v in vs:
+                v_sum = v_sum + v
+                if v >= target_r:
+                    ex_profit += v - target_r
+                else:
+                    nex_profit = target_r - v
+
+            print i + 1, target_r, p, v_sum / len(vs), ex_profit / len(vs), nex_profit / len(vs)
+
+
+    #risk_levels = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
     #risk_levels = [8, 8, 8, 7, 7, 7, 6, 6, 6, 6]
     #risk_levels = [6, 6, 6, 6, 3, 3, 3, 3, 2, 2]
-    prob(df, target_r, risk_levels, n , interval)
+    #prob(df, target_r, risk_levels, n , interval)
     #print risk_levels
