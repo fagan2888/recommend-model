5d0aa2c20d604ee6e5eb52c53793c5219b71a6d5
jiaoyangneal <yjiaoneal@163.com>
2017-04-23 16:04:22 +0800

    add code

--- cppi/shell/cppi.py
+++ cppi/shell/cppi.py
@@ -7,4 +7,32 @@ import numpy as np
 if __name__ == '__main__':
 
     df = pd.read_csv('./data/nav.csv', index_col = ['date'], parse_dates = ['date'])
-    print df['risk1']
+
+    df = df.resample('M').last()
+
+    low_df = df['risk1']
+    high_df = df['risk10']
+
+    #compute low_df three month return
+    three_month_return = []
+    dates = low_df.index
+    for i in range(0, len(dates) - 12):
+        startv = low_df.loc[dates[i]]
+        endv = low_df.loc[dates[i + 12]]
+        three_month_return.append( endv / startv - 1)
+
+
+    three_month_return = []
+    dates = high_df.index
+    for i in range(0, len(dates) - 3):
+        startv = high_df.loc[dates[i]]
+        endv = high_df.loc[dates[i + 3]]
+        three_month_return.append( endv / startv - 1)
+        print dates[i], endv / startv - 1
+    #print np.percentile(three_month_return, 5)
+    #print np.mean(three_month_return)
+    #print three_month_return
+
+    #low_df =
+    #print low_df
+    #print high_df
