3e04e99b251e061c24cd91d3593a8cd331e6899b
jiaoyangneal <yjiaoneal@163.com>
2017-04-26 14:06:28 +0800

    add code

--- cppi/shell/cppi.py
+++ cppi/shell/cppi.py
@@ -8,7 +8,7 @@ import numpy as np
 if __name__ == '__main__':
 
     df = pd.read_csv('./data/nav.csv', index_col = ['date'], parse_dates = ['date'])
-
+    df = df.iloc[-400:]
     df = df.resample('W-FRI').last()
 
     low_df = df['risk1']
@@ -17,34 +17,11 @@ if __name__ == '__main__':
     low_dfr = low_df.pct_change().fillna(0.0)
     high_dfr = high_df.pct_change().fillna(0.0)
 
-    np.percentile(low_dfr.values, 50)
-    np.percentile(high_dfr.values, 5)
-
-
-    '''
-    three_month_return = []
-    dates = low_df.index
-    for i in range(0, len(dates) - 3):
-        startv = low_df.loc[dates[i]]
-        endv = low_df.loc[dates[i + 3]]
-        three_month_return.append( endv / startv - 1)
-    low_return = np.percentile(three_month_return, 50)
-
-
-    three_month_return = []
-    dates = high_df.index
-    for i in range(0, len(dates) - 3):
-        startv = high_df.loc[dates[i]]
-        endv = high_df.loc[dates[i + 3]]
-        three_month_return.append( endv / startv - 1)
-    high_drawdown = np.percentile(three_month_return, 5)
-    '''
+    low_return = np.percentile(low_dfr.values, 50)
+    #high_drawdown = np.percentile(high_dfr.values, 5)
 
     #print low_return, high_drawdown
-
-
-    low_df = low_df.iloc[::3]
-    high_df = high_df.iloc[::3]
+    #print low_return, high_drawdown
 
     dates = low_df.index
     high_dfr = high_df.pct_change().fillna(0.0)
@@ -56,6 +33,7 @@ if __name__ == '__main__':
     highw = 0
     loww = 0
     ds = []
+    pos = []
     for i in range(0, len(dates)):
         d = dates[i]
         if len(vs) == 0:
@@ -63,17 +41,26 @@ if __name__ == '__main__':
         else:
             v = vs[-1]
 
-        high = (v * low_return) * multi_v
+        high = (v * (1 + low_return) - 1) * multi_v
+        if high > v:
+            high = v
+        elif high < 0:
+            high = 0
         low = v - high
 
         highr = high_dfr.loc[d]
         lowr = low_dfr.loc[d]
 
-        vs.append(high * (1 + highr) + low * (1 + lowr))
+        now_v = high * (1 + highr) + low * (1 + lowr)
+        pos.append(high / v)
+        vs.append(now_v)
         ds.append(d)
+        print d, high / v, now_v
 
-    df = pd.DataFrame(vs, index = ds, columns = ['nav'])
+    df = pd.DataFrame(np.matrix([pos, vs]).T, index = ds, columns = ['pos', 'nav'])
     df.to_csv('cppi.csv')
+
+
     #print np.mean(three_month_return)
     #print three_month_return
 
