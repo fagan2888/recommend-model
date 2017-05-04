1900f23da9c053900d1715a3482c6bbcb960283f
jiaoyangneal <yjiaoneal@163.com>
2017-05-04 09:47:37 +0800

    add cppi

--- cppi/shell/cppi.py
+++ cppi/shell/cppi.py
@@ -96,14 +96,31 @@ def filter_by_turnover(df, turnover):
     return pd.DataFrame(result).T
 
 
+def combine_pos(cppi_pos_df, high_risk_pos_df, low_risk_pos_df, high_risk, low_risk):
+
+    cppi_pos_df = result_df[[high_risk, low_risk]]
+
+    low_risk_pos_df = low_risk_pos_df.reindex(cppi_pos_df.index)
+    low_risk_pos_df = low_risk_pos_df.fillna(method = 'pad')
+    high_risk_pos_df = high_risk_pos_df.reindex(cppi_pos_df.index)
+    high_risk_pos_df = high_risk_pos_df.fillna(method = 'pad')
+
+    low_risk_pos_df = pd.DataFrame(np.multiply(low_risk_pos_df.values, cppi_pos_df[[low_risk]].values), index = low_risk_pos_df.index, columns = low_risk_pos_df.columns)
+    high_risk_pos_df = pd.DataFrame(np.multiply(high_risk_pos_df.values, cppi_pos_df[[high_risk]].values), index = high_risk_pos_df.index, columns = high_risk_pos_df.columns)
+
+    risk_pos_df = pd.concat([high_risk_pos_df, low_risk_pos_df], axis = 1)
+    return risk_pos_df
+
+
 if __name__ == '__main__':
 
 
-    risk1_pos_df = pd.read_csv('./data/risk1_pos.csv', index_col = ['date'], parse_dates = ['date'])
-    risk10_pos_df = pd.read_csv('./data/risk10_pos.csv', index_col = ['date'], parse_dates = ['date'])
     #print risk1_pos_df
     #print risk10_pos_df
     #print risk10_pos_df
+
+    risk1_pos_df = pd.read_csv('./data/risk1_pos.csv', index_col = ['date'], parse_dates = ['date'])
+    risk10_pos_df = pd.read_csv('./data/risk10_pos.csv', index_col = ['date'], parse_dates = ['date'])
 
     df = pd.read_csv('./data/nav.csv', index_col = ['date'], parse_dates = ['date'])
     #df = df.iloc[-400:]
@@ -121,6 +138,7 @@ if __name__ == '__main__':
     num = 0
     total = 0
     rs = []
+
 
     '''
     for i in range(0, len(dates) - 12):
@@ -144,14 +162,14 @@ if __name__ == '__main__':
     '''
 
     result_df = cppi(df, high_risk, low_risk, max_loss, multi_v, turnover, weeks)
+    result_df.to_csv('result.csv')
+    pos_df = combine_pos(result_df[[high_risk, low_risk]], risk10_pos_df, risk1_pos_df, high_risk, low_risk)
     #print result_df
-    cppi_pos_df = result_df[['risk1', 'risk10']]
-    #print cppi_pos_df.index
-    #print risk1_pos_df.index
-    cppi_pos_df.reindex(risk1_pos_df.index)
-    cppi_pos_df = cppi_pos_df.fillna(method = 'pad')
-    #risk1_pos_df = risk1_pos_df * cppi_pos_df[['risk1']]
-    print risk1_pos_df
-    print cppi_pos_df[['risk1']]
-    #print cppi_pos_df
-
+    turnover = pos_df.diff().abs().sum().sum()
+    print turnover
+    #for i in range(0, len(dates) - 12):
+    #    tmp_df = df.iloc[ i : ]
+    #    result_df = cppi(tmp_df, high_risk, low_risk, max_loss, multi_v, turnover, weeks)
+    #    pos_df = combine_pos(result_df[[high_risk, low_risk]], risk10_pos_df, risk1_pos_df, high_risk, low_risk)
+    #    turnover = pos_df.diff().abs().sum().sum()
+    #    print dates[i], turnover
