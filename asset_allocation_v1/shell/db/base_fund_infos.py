3b284a581ad634b92b2b67f62998f77dbba85540
Likun Liu <liulikun@gmail.com>
2017-05-09 12:20:55 +0800

    redeem order ok

--- asset_allocation_v1/shell/db/base_fund_infos.py
+++ asset_allocation_v1/shell/db/base_fund_infos.py
@@ -35,7 +35,7 @@ def load_ack(gids=None, codes=None, xtype=5):
     df = pd.read_sql(s, db, index_col=['fi_globalid'])
 
     df['buy'] = df['fi_yingmi_confirm_time'] + 1 # 实际中，购买完成，份额可赎回时间是订单确认时间 + 1天
-    sr = df['fi_yingmi_to_account_time'] - 2
+    sr = df['fi_yingmi_to_account_time'] - 2     # 实际中，赎回到盈米宝的到账时间是赎回到银行卡的到账时间 - 2天
     df['redeem'] = sr.where(sr >= 1, 1)
     df['code'] = df['fi_code'].apply(lambda x: "%06d" % x)
 
