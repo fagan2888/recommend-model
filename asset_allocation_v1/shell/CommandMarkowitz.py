ad7a4a67e63e64b9fa9469689e230b25065b3469
yjiaoneal <yjiaoneal@163.com>
2017-06-01 12:27:45 +0800

    add code

--- asset_allocation_v1/shell/CommandMarkowitz.py
+++ asset_allocation_v1/shell/CommandMarkowitz.py
@@ -258,7 +258,7 @@ def allocate(ctx, optid, optname, opttype, optreplace, startdate, enddate, lookb
         if short_cut == 'high':
             assets = {
                 120000001:  {'sum1': 0,    'sum2' : 0,   'upper': 0.70,  'lower': 0.0}, #沪深300指数修型
-                120000002:  {'sum1': 0,    'sum2' : 0,   'upper': 0.70,  'lower': 0.0}, #中证500指数修型
+                #120000002:  {'sum1': 0,    'sum2' : 0,   'upper': 0.70,  'lower': 0.0}, #中证500指数修型
                 120000013:  {'sum1': 0.65, 'sum2' : 0,   'upper': 0.35, 'lower': 0.0}, #标普500指数
                 120000015:  {'sum1': 0.65, 'sum2' : 0,   'upper': 0.35, 'lower': 0.0}, #恒生指数修型
                 120000014:  {'sum1': 0.65, 'sum2' : 0.45,'upper': 0.35, 'lower': 0.0}, #黄金指数修型
@@ -271,7 +271,7 @@ def allocate(ctx, optid, optname, opttype, optreplace, startdate, enddate, lookb
         elif short_cut == 'low':
             assets = {
                 120000010:  {'sum1': 0, 'sum2': 0, 'upper': 1.0, 'lower': 0.0},
-                120000011:  {'sum1': 0, 'sum2': 0, 'upper': 1.0, 'lower': 0.0},
+                #120000011:  {'sum1': 0, 'sum2': 0, 'upper': 1.0, 'lower': 0.0},
             }
             if optname is None:
                 optname = u'马克维茨%s(低风险)' % today.strftime("%m%d")
