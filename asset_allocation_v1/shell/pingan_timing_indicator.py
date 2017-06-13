0beed20888be59763a2cd988d5cacd44030a07c9
yjiaoneal <yjiaoneal@163.com>
2017-06-13 14:14:49 +0800

    add code

--- asset_allocation_v1/shell/pingan_timing_indicator.py
+++ asset_allocation_v1/shell/pingan_timing_indicator.py
@@ -10,21 +10,21 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    df = pd.read_csv('./data/pingan_timing.csv', index_col = ['date'], parse_dates = ['date'])
+    df = pd.read_csv('./export_nav.csv', index_col = ['date'], parse_dates = ['date'])
 
-
-    data = df['mid_small_t']
-    base = df['mid_small']
+    data = df['92101:12:9']
+    base = df['zz500']
 
     data_r = data.pct_change().fillna(0.0)
     base_r = base.pct_change().fillna(0.0)
 
-    rf = 0.03 / 365
-    sharp = (np.mean(data_r) * 365 - rf * 365) / (np.std(data_r) * (365 ** 0.5))
+    rf = 0.03 / 252
+    print 'annual' , (np.mean(data_r)) * 252
+    sharp = (np.mean(data_r) * 252 - rf * 252) / (np.std(data_r) * (252 ** 0.5))
     print 'sharp', sharp
-    std = np.std(data_r) * (360 ** 0.5)
+    std = np.std(data_r) * (252 ** 0.5)
     print 'std', std
-    print 'jensen', fin.jensen(data_r, base_r, rf) * 365
+    print 'jensen', fin.jensen(data_r, base_r, rf) * 252
     print 'semivariance', fin.semivariance(data_r)
     print 'treynor', fin.treynor(data_r, base_r, rf)
     print 'ir', fin.ir(data_r, base_r)
