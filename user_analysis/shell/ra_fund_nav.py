b3957a04af05e1fb793b4bd7df797919019d555a
jiaoyangneal <yjiaoneal@163.com>
2017-06-27 10:56:34 +0800

    add user earning

---
+++ user_analysis/shell/ra_fund_nav.py
@@ -0,0 +1,49 @@
+#coding=utf8
+
+
+import MySQLdb
+import pandas as pd
+import numpy as np
+import datetime
+
+
+mofang = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "passwd": "Mofang123",
+    "db":"mofang",
+    "charset": "utf8"
+}
+
+
+if __name__ == '__main__':
+
+    conn  = MySQLdb.connect(**mofang)
+    conn.autocommit(True)
+
+    sql = 'select td_date from trade_dates'
+    df = pd.read_sql(sql, conn)
+    dates = []
+    for d in df.values:
+        dates.append(d[0].strftime('%Y-%m-%d'))
+
+    sql = 'select ra_code as code from ra_fund where (ra_type = 1 or ra_type = 4) and ra_mask = 0'
+    df = pd.read_sql(sql, conn)
+    codes = ','.join(["'" + code[0] + "'" for code in df.values])
+
+    sql = 'select ra_code as code, ra_date as date, ra_nav_adjusted as nav from ra_fund_nav where ra_code in (' + codes + ')'
+
+    df = pd.read_sql(sql, conn, index_col = ['date', 'code'])
+    df = df.unstack()
+    df.index = df.index.astype(str)
+
+    dates = set(dates) & set(df.index)
+    dates = list(dates)
+    dates.sort()
+    df = df.loc[dates]
+    #df = df[~df.index.duplicated(keep = 'first')]
+    #df = df.unstack()
+
+    print df
+    df.to_csv('./data/nav.csv')
--- user_analysis/shell/user_earning.py
+++ user_analysis/shell/user_earning.py
@@ -3,6 +3,7 @@
 
 import MySQLdb
 import pandas as pd
+import numpy as np
 
 
 asset_allocation = {
@@ -24,7 +25,41 @@ portfolio_statistics = {
 }
 
 
+trade = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "passwd": "Mofang123",
+    "db":"trade",
+    "charset": "utf8"
+}
+
+
+def earning(nav_df):
+
+    nav_df['io_share'] = nav_df['amount_io'] / nav_df['nav']
+    nav_df['share'] = nav_df['io_share'].shift(1).fillna(0.0).cumsum()
+    nav_df['amount'] = nav_df['share'] * nav_df['nav']
+    nav_df['amount_diff'] = nav_df['amount'].diff().fillna(0.0)
+    nav_df['earning'] = nav_df['amount_diff'] - nav_df['amount_io'].shift(1).fillna(0.0)
+
+    return nav_df
+
+
 if __name__ == '__main__':
+
+
+    conn  = MySQLdb.connect(**trade)
+    conn.autocommit(True)
+
+
+    sql = 'select mp_uid, mp_risk, mp_placed_date from mf_portfolio_trade_statuses'
+    df = pd.read_sql(sql, conn, index_col = ['mp_uid'], parse_dates = ['mp_placed_date'])
+
+    high_risk_uids = []
+    for k, v in df.groupby(df.index):
+        if v['mp_risk'].values[-1] >= 0.7:
+            high_risk_uids.append(k)
 
 
     conn  = MySQLdb.connect(**portfolio_statistics)
@@ -32,7 +67,8 @@ if __name__ == '__main__':
 
 
     sql = 'select ds_uid, ds_portfolio_id, ds_trade_date, ds_trade_type, ds_amount from ds_order'
-    df = pd.read_sql(sql, conn, index_col = ['ds_uid'])
+    df = pd.read_sql(sql, conn, index_col = ['ds_uid'], parse_dates = ['ds_trade_date'])
+    df = df.loc[high_risk_uids]
     conn.close()
 
     #print df['ds_trade_type'].isin([20, 21, 30, 31])
@@ -42,32 +78,33 @@ if __name__ == '__main__':
 
     df['ds_amount'] = df['ds_amount'] * mask
 
-    #for uid in df.index.unique():
-    #    if len(df.loc[uid]) > 10:
-    #        print df.loc[uid]
-
-    uid = 1000082620
-    asset_io = df.loc[uid]
+    asset_io = df.reset_index()
     asset_io = asset_io.reset_index()
     asset_io = asset_io[['ds_trade_date', 'ds_amount']]
     asset_io = asset_io.set_index('ds_trade_date')
     asset_io = asset_io.groupby(asset_io.index).sum()
 
-
+    '''
     conn  = MySQLdb.connect(**asset_allocation)
     conn.autocommit(True)
-    sql = 'select ra_date, ra_nav from ra_portfolio_nav where ra_portfolio_id = 80062600 and ra_type = 9'
+    sql = 'select ra_date, ra_nav as nav from ra_portfolio_nav where ra_portfolio_id = 80062600 and ra_type = 9'
     nav_df = pd.read_sql(sql, conn, index_col = ['ra_date'])
-
+    nav_df.index.name = 'date'
     asset_io = asset_io.reindex(nav_df.index).fillna(0.0)
-    nav_df['ds_amount'] = asset_io['ds_amount']
+    nav_df['amount_io'] = asset_io['ds_amount']
+    nav_df = earning(nav_df)
+    #nav_df.to_csv('user_nav.csv')
+    print nav_df.sum()['earning']
+    '''
 
-
-    nav_df['io_share'] = nav_df['ds_amount'] / nav_df['ra_nav']
-    nav_df['share'] = nav_df['io_share'].shift(1).fillna(0.0).cumsum()
-    nav_df['amount'] = nav_df['share'] * nav_df['ra_nav']
-    nav_df['amount_diff'] = nav_df['amount'].diff().fillna(0.0)
-    nav_df['earning'] = nav_df['amount_diff'] - nav_df['ds_amount'].shift(1).fillna(0.0)
-    print nav_df
-
-    nav_df.to_csv('user_nav.csv')
+    df = pd.read_csv('./data/nav.csv', index_col = ['date'], parse_dates = ['date'])
+    df = df[df.index >= '2016-05-01']
+    df = df.dropna(axis = 1)
+    asset_io = asset_io.reindex(df.index)
+    for code in df.columns:
+        nav_df = df[[code]]
+        nav_df.columns = ['nav']
+        nav_df['amount_io'] = asset_io['ds_amount']
+        nav_df = earning(nav_df)
+        #nav_df.to_csv('user_nav.csv')
+        print code, ',', nav_df.sum()['earning']
