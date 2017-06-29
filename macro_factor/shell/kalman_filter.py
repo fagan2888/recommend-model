ec9a216e9c0c06036ec23ac6a985bb18f8f1390e
yjiaoneal <yjiaoneal@163.com>
2017-06-29 17:12:05 +0800

    add code

--- macro_factor/shell/kalman_filter.py
+++ macro_factor/shell/kalman_filter.py
@@ -7,15 +7,28 @@ from pykalman import KalmanFilter
 if __name__ == '__main__':
 
 
-    index_df = pd.read_csv('./data/m1_m2_yoy.csv', index_col = ['date'], parse_dates = ['date'])
+    index_df = pd.read_csv('./data/index.csv', index_col = ['date'], parse_dates = ['date'])
     #m1_m2_df = money_df.fillna(method = 'pad')
     #m1_m2_df = m1_m2_df.resample('M', how = 'last')
     #m1_m2_df = m1_m2_df.iloc[0 : -2]
-
-    hs = index_df['000001.SH']
+    hs = index_df['000300.SH']
     dates = []
     vs = []
-    for i in range(252 : len(hs)):
+    for i in range(252 , len(hs)):
+
+        d = hs.index[i]
+        tmp_vs = hs.iloc[ i - 252 : i + 1]
+
+        kf = KalmanFilter(initial_state_mean = 0, n_dim_obs = 1)
+        (us, vars) = kf.em(tmp_vs.values).filter(tmp_vs.values)
+        dates.append(d)
+        vs.append([tmp_vs.values[-1], us[-1][0]])
+        print d, tmp_vs.values[-1], us[-1][0]
+
+    df = pd.DataFrame(vs, index = dates, columns = ['000300.SH', 'kf'])
+    df.index.name = 'date'
+    #print df
+    df.to_csv('tmp.csv')
 
     #m1_m2_yoy_vs = m1_m2_df['m1_m2_yoy'].values
     #hs000001_vs = m1_m2_df['000001.SH'].values
--- macro_factor/shell/m1_m2.py
+++ macro_factor/shell/m1_m2.py
@@ -1,17 +1,26 @@
 #coding=utf8
 
+
 import pandas as pd
 from pykalman import KalmanFilter
+import numpy as np
 
 
 if __name__ == '__main__':
 
+
     m1_m2_df = pd.read_csv('./data/m1_m2_yoy.csv', index_col = ['date'], parse_dates = ['date'])
     #m1_m2_df = money_df.fillna(method = 'pad')
     m1_m2_df = m1_m2_df.resample('M', how = 'last')
-    m1_m2_df = m1_m2_df.iloc[0 : -2]
+    #m1_m2_df = m1_m2_df.iloc[0 : -2]
 
 
+    df = m1_m2_df[['000001.SH', 'm1_m2_yoy']]
+    #print  df.corr()
+    #print vs
+
+
+    '''
     m1_m2_yoy_vs = m1_m2_df['m1_m2_yoy'].values
     hs000001_vs = m1_m2_df['000001.SH'].values
     kf = KalmanFilter(initial_state_mean = 0, n_dim_obs = 1)
@@ -20,4 +29,5 @@ if __name__ == '__main__':
     (us, vs) = kf.em(hs000001_vs).filter(hs000001_vs)
     m1_m2_df['000001.SH_kf'] = us
     print m1_m2_df
+    '''
     #m1_m2_df.to_csv('tmp.csv')
