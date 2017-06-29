8e7053dedf49003f5ffc04d877c0578c3bcbc551
Likun Liu <liulikun@gmail.com>
2017-06-29 15:25:55 +0800

    bug fixed

--- asset_allocation_v1/shell/db/__init__.py
+++ asset_allocation_v1/shell/db/__init__.py
@@ -14,8 +14,10 @@ import asset_mz_markowitz_pos
 import asset_mz_markowitz_sharpe
 import asset_on_online
 import asset_on_online_alloc
+import asset_on_online_criteria
 import asset_on_online_fund
 import asset_on_online_nav
+import asset_on_online_pos
 import asset_on_online_contrib
 import asset_ra_pool
 import asset_ra_pool_fund
