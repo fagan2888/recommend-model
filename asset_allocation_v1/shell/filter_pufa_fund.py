426e37f41e425bd356d64e2a42045dfb10da756e
yjiaoneal <yjiaoneal@163.com>
2017-07-22 13:41:30 +0800

    add filter pufa fund code

--- asset_allocation_v1/shell/filter_pufa_fund.py
+++ asset_allocation_v1/shell/filter_pufa_fund.py
@@ -6,18 +6,37 @@ sys.path.append('shell')
 import MySQLdb
 import config
 import pandas as pd
+import re
 
 
 
 if __name__ == '__main__':
 
 
+    pattern = re.compile("[A-Za-z]")
+    df = pd.read_csv('./data/all_pufa_fund.csv')
+    codes = []
+    for code in df.values:
+        code = code[0]
+        match = pattern.search(code)
+        if not match:
+            code = "%06d" % int(code)
+            codes.append(code)
+    codes = set(codes)
+
+
     conn  = MySQLdb.connect(**config.db_base)
     cur   = conn.cursor(MySQLdb.cursors.DictCursor)
     conn.autocommit(True)
 
-
     sql = 'select globalid, ra_code, ra_mask from ra_fund'
     df = pd.read_sql(sql, conn, index_col = ['globalid'])
 
-    df = pd.read_csv()
+    for gid in df.index:
+        code = df.loc[gid, 'ra_code']
+        if code not in codes:
+            sql = 'update ra_fund set ra_mask = 1 where globalid = %d' % gid
+            print sql
+            cur.execute(sql)
+
+    conn.close()
