821851ff306fdcf4c0ba9235be4d89c84947917c
yjiaoneal <yjiaoneal@163.com>
2017-07-22 17:47:38 +0800

    add code

--- asset_allocation_v1/shell/import_bank_fin.py
+++ asset_allocation_v1/shell/import_bank_fin.py
@@ -13,26 +13,39 @@ from datetime import datetime
 if __name__ == '__main__':
 
     f_path = sys.argv[1]
-
     df = pd.read_csv(f_path, index_col = ['date'], parse_dates = ['date'])
+
 
     conn  = MySQLdb.connect(**config.db_base)
     cur   = conn.cursor(MySQLdb.cursors.DictCursor)
     conn.autocommit(True)
 
-    sql = 'select max(globalid) from ra_fund'
+
+    sql = 'select max(fi_globalid) from fund_infos'
     cur.execute(sql)
     record = cur.fetchone()
-    gid = record['max(globalid)']
+    gid = record['max(fi_globalid)']
+
 
     code_gid_dict = {}
     for code in df.columns:
         gid = gid + 1
-        code_git_dict[code] = gid
+        code_gid_dict[code] = gid
         begin_date = df.index[0]
-        sql = "replace into ra_fund (globalid, ra_code, ra_name, ra_type, ra_type_calc, ra_regtime, ra_mask, created_at, updated_at) values (%d, '%s', '%s', 2, 1, '%s', 0, '%s', '%s')" % (gid, code, 'pufa_bank_' + str(code), begin_date, datetime.now(), datetime.now())
+        sql = "replace into fund_infos (fi_globalid, fi_code, fi_name, fi_regtime) values (%d, '%s', '%s', '%s')" % (gid, code, 'pfb_' + str(code), begin_date)
+        print sql
         cur.execute(sql)
 
 
+
+    nav_base_sql = "replace into caihui_spider_fund_value (cs_fund_id, cs_code, cs_time, cs_net_value, cs_total_value, cs_authority_value, cs_status, created_at, updated_at) values ('%d', '%s', '%s', '%f', '%f', '%f', 3, '%s', '%s')"
+    for code in df.columns:
+        gid = code_gid_dict[code]
+        ser = df[code]
+        for d in ser.index:
+            v = ser.loc[d]
+            sql = nav_base_sql % (gid, code, d, v, v, v, datetime.now(), datetime.now())
+            print sql
+            cur.execute(sql)
 
     conn.close()
---
+++ asset_allocation_v1/shell/modify_fund_pool.py
@@ -0,0 +1,49 @@
+#coding=utf8
+
+
+import sys
+sys.path.append('shell')
+import MySQLdb
+import config
+import pandas as pd
+import re
+from datetime import datetime
+
+
+if __name__ == '__main__':
+
+    op        = sys.argv[1]
+    pool_id   = sys.argv[2]
+    fund_code = sys.argv[3]
+
+
+    conn  = MySQLdb.connect(**config.db_base)
+    cur   = conn.cursor(MySQLdb.cursors.DictCursor)
+    conn.autocommit(True)
+
+
+    sql = 'select globalid from ra_fund where ra_code = %s' % fund_code
+    cur.execute(sql)
+    record = cur.fetchone()
+    gid = record['globalid']
+
+    conn.close()
+
+
+    conn  = MySQLdb.connect(**config.db_asset)
+    cur   = conn.cursor(MySQLdb.cursors.DictCursor)
+    conn.autocommit(True)
+    if op == 'delete':
+        sql = "delete from ra_pool_fund where ra_pool = %d and ra_fund_id = %d" % (int(pool_id), int(gid))
+        print sql
+    elif op == 'insert':
+        sql = 'select ra_date from ra_pool_fund where ra_pool = %d' % (int)(pool_id)
+        cur.execute(sql)
+        dates = []
+        records = cur.fetchall()
+        for record in records:
+            dates.append(record['ra_date'])
+        dates = set(dates)
+
+        for d in dates:
+            sql = 'replace into ra_pool_fund (ra_pool, ra_category, ra_date, ra_fund_id, ra_fund_code, ra_fund_type, ra_fund_level, ra_mask, created_at, updated_at) values ()'
