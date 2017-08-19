148f3ed639a86ce742e224e237e5187319ea34db
yaojiahui <xzcbvnz@163.com>
2017-08-19 14:59:38 +0800

    add predict to cal_GMX

--- asset_allocation_v1/shell/cal_GMX.py
+++ asset_allocation_v1/shell/cal_GMX.py
@@ -4,10 +4,13 @@ from utils import day_2_week_ipo as d2w
 from db import asset_trade_dates as load_td
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
 from scipy.stats import spearmanr
 
 def load_data():
-    sh300_indic = pd.read_csv('sh300_indic.csv', index_col = 0, parse_dates = True)
+    cols = range(15)
+    sh300_indic = pd.read_csv('sh300_indic.csv', index_col = 0, parse_dates = True, \
+            usecols = cols[:2]+cols[5:])
     ipo_data = pd.read_csv('ipo_data.csv', index_col = 0, parse_dates = True)
     trade_dates = load_td.load_trade_dates()
     ipo_data = d2w(ipo_data, trade_dates)
@@ -27,7 +30,7 @@ def pca():
             'c5'], index = data.index)
     print np.corrcoef(df_components.c1, data.pct_chg1)[0,1]
 
-def predict():
+def cal_components():
     window = 50
     data = load_data()
     c1_list = []
@@ -64,6 +67,7 @@ def predict():
 
 def res_sta():
     data = pd.read_csv('sh300_gmx.csv')
+    #output = open('result_gmx.csv', 'w')
     for i in range(1, 6):
         corr_close = np.corrcoef(data['c%d'%i], data['close'])[0,1]
         corr_pct_chg = np.corrcoef(data['c%d'%i], data['pct_chg'])[0,1]
@@ -71,6 +75,8 @@ def res_sta():
         rcorr_close = spearmanr(data['c%d'%i], data['close']).correlation
         rcorr_pct_chg = spearmanr(data['c%d'%i], data['pct_chg']).correlation
         rcorr_pct_chg1 = spearmanr(data['c%d'%i], data['pct_chg1']).correlation
+        #output.write('%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f\n'%(corr_close, rcorr_close,\
+        #        corr_pct_chg, rcorr_pct_chg, corr_pct_chg1, rcorr_pct_chg1))
         print '###############################'
         print 'c%d'%i
         print corr_close
@@ -80,6 +86,43 @@ def res_sta():
         print rcorr_pct_chg
         print rcorr_pct_chg1
 
+def cal_wr(x, y):
+    '''
+    x: pre,
+    y: real
+    '''
+    total = 0
+    correct = 0
+    for (i,j) in zip(x, y):
+        if i == -1:
+            total+=1.0
+            if j == -1:
+                correct += 1.0
+    print total
+    print correct/total
+
+def predict():
+    data = pd.read_csv('sh300_gmx.csv', index_col = 0, parse_dates = True)
+    data['signal'] = np.sign(data['pct_chg1']*data['pct_chg'])
+    pre_signals = []
+    window = 50
+    for i in range(len(data) - window):
+        train_x = data.ix[i: i+window, ['c1','c2','c3','c4','c5']]
+        scaler = StandardScaler()
+        train_x = scaler.fit_transform(train_x)
+        test_x = data.ix[i: i+window+1, ['c1','c2','c3','c4','c5']]
+        test_x = scaler.fit_transform(test_x)
+        train_y = data.ix[i: i+window, ['signal']]
+        clf = RandomForestClassifier(class_weight = {1: 1e10}, random_state = 0)
+        clf.fit(train_x, train_y)
+        pre_signal = clf.predict(test_x)
+        pre_signals.append(pre_signal[-1])
+
+    data = data[window:]
+    data['pre_signal'] = pre_signals
+    print cal_wr(data.pre_signal, data.signal)
+
 if __name__ == '__main__':
-    #predict()
-    res_sta()
+    #cal_components()
+    #res_sta()
+    predict()
