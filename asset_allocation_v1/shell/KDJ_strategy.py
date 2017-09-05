23a0813dfc1d50a0db247333667eda950997dbcd
wangxiaokun <2623246763@qq.com>
2017-09-05 15:46:52 +0800

    change KDJ_strategy

--- asset_allocation_v1/shell/KDJ_strategy.py
+++ asset_allocation_v1/shell/KDJ_strategy.py
@@ -8,17 +8,20 @@ class KDJ_strategy:
 
 	def timing(self, df_nav, date):
 		periodHigh = pd.Series(0.0, index = df_nav.index)
-		periodHigh = pd.rolling_max(df_nav.tc_close,window = date)
+		periodHigh = pd.rolling_max(df_nav.tc_high,window = date)
 		periodLow = pd.Series(0.0, index = df_nav.index)
-		periodLow = pd.rolling_min(df_nav.tc_close,window = date)
+		periodLow = pd.rolling_min(df_nav.tc_low,window = date)
 		rsv = pd.Series(0.0, index = df_nav.index)
 		k_value = pd.Series(0.0, index = df_nav.index)
 		d_value = pd.Series(0.0, index = df_nav.index)
 		for i in range(8, len(df_nav)):
 			rsv[i] = ((df_nav.tc_close[i] - periodLow[i])/(periodHigh[i] - periodLow[i]))*100
 		for i in range(8, len(df_nav)):
-			k_value[i] = 0.67*k_value[i-1]+0.33*rsv[i]
-			d_value[i] = 0.67*d_value[i-1]+0.33*k_value[i]
+			k_value[i] = (2.0/3)*k_value[i-1]+(1.0/3)*rsv[i]
+			d_value[i] = (2.0/3)*d_value[i-1]+(1.0/3)*k_value[i]
+		print df_nav.tc_close.tail()
+		print k_value.tail()
+		print d_value.tail()
 		singnal = pd.Series(0.0, index = df_nav.index)
 		for j in range(8, len(df_nav)):
 			if k_value[j-1]<d_value[j-1] and k_value[j]>d_value[j]:
