51bf07e05da653afabcd8f8902d15b1ec1457b37
jiaoyangneal <yjiaoneal@163.com>
2017-09-11 16:39:24 +0800

    add code

--- user_analysis/shell/add_money_analysis.py
+++ user_analysis/shell/add_money_analysis.py
@@ -3,31 +3,36 @@
 import numpy as np
 import pandas as pd
 
+
 if __name__ == '__main__':
 
 
-    a_month_day = 22
+    a_month_day = 5
+    #code = 'nav'
     code = '000001.SH'
     df = pd.read_csv('./data/nav.csv', index_col = ['date'])
     df = df[[code]]
+    #print df / df.iloc[0]
     df['cummax'] = df[code].cummax()
     df['drawdown'] = 1.0 - df[code] / df['cummax']
     df_inc = df
     df_inc[code] = df_inc[code].pct_change().fillna(0.0)
 
     #df_inc = df.pct_change().fillna(0.0)
-    df_inc['22d'] = df_inc[code].rolling(window = a_month_day).sum()
-    df_inc['22d'] = df_inc['22d'].shift(-1 * a_month_day)
+    df_inc['a_month_day'] = df_inc[code].rolling(window = a_month_day).sum()
+    df_inc['a_month_day'] = df_inc['a_month_day'].shift(-1 * a_month_day)
     df_inc = df_inc.dropna()
 
     dates = df_inc.index
     all_r = []
     for i in range(0, len(dates)):
-        #rs = df_inc[code][i - 5 : i ]
+        rs = df_inc[code][i - 0 : i ]
         #if rs[0] < 0 and rs[1] < 0 and rs[2] < 0 and rs[3] < 0 and rs[4] < 0:
+        #if rs[0] < 0 and rs[1] < 0 and rs[2] < 0:
         if df.loc[dates[i] , 'drawdown'] > 0.01:
-            r = df_inc['22d'][i]
+            r = df_inc['a_month_day'][i]
             all_r.append(r)
     rs = np.array(all_r)
     print rs.mean()
     print 1.0 * (rs > 0).sum() / len(rs)
+    print len(rs)
--- user_analysis/shell/export_online_nav.py
+++ user_analysis/shell/export_online_nav.py
@@ -20,7 +20,12 @@ if __name__ == '__main__':
     conn  = MySQLdb.connect(**asset_allocation)
     conn.autocommit(True)
 
-    sql = 'select on_date as date, on_nav as nav from on_online_nav where on_online_id = 800001 and on_type = 8'
-    df = pd.read_sql(sql, conn, index_col = ['date'])
-    print df
-    df.to_csv('./tmp/online_nav.csv')
+    sql = 'select on_date as date, on_nav as nav from on_online_nav where on_online_id = 800000 and on_type = 8'
+    online_df = pd.read_sql(sql, conn, index_col = ['date'])
+    online_df.to_csv('./tmp/online_nav.csv')
+
+
+    df = pd.read_csv('data/000001.SH.csv', parse_dates = ['date'], index_col = ['date'])
+    df = pd.concat([online_df, df], axis = 1, join_axes = [df.index])
+    df = df.dropna()
+    df.to_csv('./data/nav.csv')
