823a29bf5c2868b05cbb29251cd43fe98425e192
yjiaoneal <yjiaoneal@163.com>
2017-09-20 07:30:51 +0800

    add code

--- asset_allocation_v1/shell/db/base_ra_fund_nav.py
+++ asset_allocation_v1/shell/db/base_ra_fund_nav.py
@@ -86,20 +86,20 @@ def load_series(code, reindex=None, begin_date=None, end_date=None):
         t1.c.ra_nav_adjusted.label('nav'),
     ]
 
-    s = select(columns) \
-        .where((t1.c.ra_code == code) | (t1.c.ra_fund_id == code))
+    if code / 10000000 == 3:
+        s = select(columns).where(t1.c.ra_fund_id == code)
+    else:
+        s = select(columns).where(t1.c.ra_code == code)
 
-    # s = select(columns).where(t1.c.ra_fund_id == id_)
     if begin_date is not None:
         s = s.where(t1.c.ra_date >= begin_date)
     if end_date is not None:
         s = s.where(t1.c.ra_date <= end_date)
-        
+
     df = pd.read_sql(s, db, index_col = ['date'], parse_dates=['date'])
 
     if reindex is not None:
         df = df.reindex(reindex, method='pad')
-
     return df['nav']
 
 
