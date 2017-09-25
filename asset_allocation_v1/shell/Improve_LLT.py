0c288ecf0e3199cb48a31e7589fd59bf0a70e710
wangxiaokun <2623246763@qq.com>
2017-09-25 16:40:39 +0800

    volume_strategy

---
+++ asset_allocation_v1/shell/Improve_LLT.py
@@ -0,0 +1,53 @@
+#coding=utf8
+
+
+import pandas as pd
+import numpy as np
+
+
+class LLTStrategy(object):
+
+    #设置a为权重指数
+    def __init__(self,d):
+        self.a = 2.0/(d+1)
+
+
+    #列出每日llt的值
+    def llt(self,df_nav,a):
+        Llt = pd.DataFrame({'LLT':0.0}, index = df_nav.index)
+        num = len(df_nav)
+        for i in range(2, num):
+        #   Llt[i] = (a-(a*a)/4)*df_nav.iloc[i][3]+((a**2)*Close_price.iloc[i-1]\
+        #   -(a-(3*a**2)/4)*Close_price.iloc[i-2]+2*(1-a)*Llt[i-1]-(1-a)**2*Llt[i-2]
+            Llt.iloc[i][0] = (a-(a*a)/4)*df_nav.iloc[i][3]+((a*a)/2)*df_nav.iloc[i-1][3]\
+                     -(a-3*(a*a)/4)*df_nav.iloc[i-2][3]+2*(1-a)*Llt.iloc[i-1][0]-(1-a)*\
+                     (1-a)*Llt.iloc[i-2][0]
+        return Llt
+
+
+    # 择时：
+    #获得llt在当日该点切线k
+    # k < 0 singnal=-1 通知用户赎回
+    # k > 0 singnal=1  通知用户购买
+    # k = 0 singnal=0  维持原状
+    def signal(self,Llt):
+        Signal = pd.DataFrame({'tc_signal':[0]*len(Llt)}, index = Llt.index)
+        Llt = Llt.diff()
+        num = len(Llt)
+        for i in range(1, num):
+            if Llt.iloc[i][0] < 0:
+                Signal.iloc[i][0] = -1
+            elif Llt.iloc[i][0] > 0:
+                Signal.iloc[i][0] = 1
+        return Signal
+
+
+
+    def timing(self,df_nav):
+        Llt = self.llt(df_nav, self.a)
+        Signal = self.signal(Llt)
+        Signal.replace(0,np.nan, inplace = True)
+        Signal.fillna(method='ffill',inplace = True)
+        Signal.dropna(inplace = True)
+        return Signal
+
--- asset_allocation_v1/shell/Volume_strategy.py
+++ asset_allocation_v1/shell/Volume_strategy.py
@@ -8,39 +8,25 @@ class Volume_strategy(object):
 
 	#计算5日成交平均值，10日成交平均值
 	#将两个值进行平均
-	def compute_volume(self, df_volume):
-		volume = pd.DataFrame({'volume':df_volume.tc_volume}, index = df_volume.index)
-<<<<<<< HEAD
-		volSMA5 = volume.rolling(window=25).mean()
-		volSMA10 = volume.rolling(window=60).mean()
-		volSMA = pd.DataFrame({'volSMA5':volSMA5.volume, 'volSMA10':volSMA10.volume,\
-							 'volSMA':(volSMA5.volume+volSMA10.volume)/2, 'volume':\
-							 df_volume.tc_volume},columns=['volSMA5','volSMA10','volSMA','volume'] ,\
-							 index=df_volume.index)
-=======
-		volSMA5 = volume.rolling(window=5).mean()
-		volSMA10 = volume.rolling(window=10).mean()
-		volSMA = pd.DataFrame({'volSMA5':volSMA5.volume, 'volSMA10':volSMA10.volume,\
-							 'volSMA':(volSMA5.volume+volSMA10.volume)/2, 'volume':\
-							 df_volume.tc_volume}, index=df_volume.index)
->>>>>>> e30055599ed7bf9ba59fe0e6dc4d69396e135666
-		return volSMA
+    def compute_volume(self, df_nav):
+        volume = pd.DataFrame({'volume':df_nav.tc_volume}, index = df_nav.index)
+        volSMA5 = volume.rolling(window=5).mean()
+        volSMA10 = volume.rolling(window=10).mean()
+        volSMA = pd.DataFrame({'volSMA5':volSMA5.volume, 'volSMA10':volSMA10.volume,\
+							   'volume': df_nav.tc_volume,\
+                               'volSMA':(volSMA5.volume+volSMA10.volume)/2},\
+                              columns=['volSMA5','volSMA10','volume','volSMA'],\
+							  index=df_nav.index)
+        return volSMA
 
 	#计算5日收盘价均值，20日收盘价均值
-	def compute_price(self, df_volume):
-		close_price = pd.DataFrame({'tc_close':df_volume.tc_close}, index = df_volume.index)
-<<<<<<< HEAD
-		avg_5 = close_price.rolling(window=5).mean()
-		avg_20 = close_price.rolling(window=10).mean()
-		close_avg = pd.DataFrame({'avg_5':avg_5.tc_close, 'avg_20':avg_20.tc_close}, \
-								columns=['avg_5','avg_20'],\
-=======
-		avg_5 = close_price.rolling(window=20).mean()
-		avg_20 = close_price.rolling(window=40).mean()
-		close_avg = pd.DataFrame({'avg_5':avg_5.tc_close, 'avg_20':avg_20.tc_close}, \
->>>>>>> e30055599ed7bf9ba59fe0e6dc4d69396e135666
-								index = close_price.index)
-		return close_avg
+    def compute_price(self, df_nav):
+        close_price = pd.DataFrame({'tc_close':df_nav.tc_close}, index = df_nav.index)
+        avg_10 = close_price.rolling(window=10).mean()
+        avg_240 = close_price.rolling(window=240).mean()
+        close_avg = pd.DataFrame({'avg_10':avg_10.tc_close, 'avg_240':avg_240.tc_close}, \
+								columns=['avg_10','avg_240'], index = df_nav.index)
+        return close_avg
 
 	#策略1:将当日成交量与volSMA比较
 	#		若当日成交量>volSMA ：释放买入信号
@@ -50,56 +36,29 @@ class Volume_strategy(object):
 	#		若5日均值向下穿过20日均值 ：释放卖出信号
 	#		两个都买入则买入
 	#		两个都卖出则卖出
-	def singnal(self, volSMA, close_avg):
-		singnal_volume = pd.DataFrame({'singnal':[0]*len(volSMA)}, index=volSMA.index)
-<<<<<<< HEAD
-		#singnal_close = pd.DataFrame({'singnal':[0]*len(close_avg)}, index=close_avg.index)
-		
-		for i in range(1, len(volSMA)):
-			if volSMA.iloc[i, 3] > volSMA.iloc[i, 2] and \
+    def signal(self, volSMA, close_avg, df_nav):
+        #num = len(close_avg)
+        signal = pd.DataFrame({'tc_signal':[0]*len(close_avg)},index = close_avg.index)
+        for i in range(1, len(close_avg)):
+            if df_nav.iloc[i, 4] > volSMA.iloc[i, 3] and \
 			   close_avg.iloc[i-1, 0] < close_avg.iloc[i-1 ,1] and \
                close_avg.iloc[i, 0] > close_avg.iloc[i, 1]:
-				singnal_volume.iloc[i,0] = 1
-			elif volSMA.iloc[i, 3] < volSMA.iloc[i ,2] and \
+                signal.iloc[i,0] = 1
+            elif df_nav.iloc[i, 4] < volSMA.iloc[i ,3] and \
 			   close_avg.iloc[i-1, 0] > close_avg.iloc[i-1 ,1] and \
                close_avg.iloc[i, 0] < close_avg.iloc[i, 1]:
-				singnal_volume.iloc[i, 0] = -1
+                signal.iloc[i,0] = -1
 
-		singnal = pd.DataFrame({'tc_signal':singnal_volume.singnal}, \
-								index = singnal_volume.index)
-		singnal.replace(0,np.nan,inplace = True)
-		singnal.fillna(method = 'ffill', inplace = True)
-=======
-		singnal_close = pd.DataFrame({'singnal':[0]*len(close_avg)}, index=close_avg.index)
+        return signal
+
+
+    def timing(self, df_nav):
 		
-		for i in range(0, len(volSMA)):
-			if volSMA.iloc[i, 3] > volSMA.iloc[i, 2]:
-				singnal_volume.iloc[i,0] = 1
-			elif volSMA.iloc[i, 3] < volSMA.iloc[i ,2]:
-				singnal_volume.iloc[i, 0] = -1
-
-		for i in range(0, len(close_avg)):
-			if close_avg.iloc[i-1, 0] < close_avg.iloc[i-1 ,1] and \
-			   close_avg.iloc[i, 0] > close_avg.iloc[i, 1]:
-				singnal_close.iloc[i, 0] = 1
-			elif close_avg.iloc[i-1, 0] > close_avg.iloc[i-1 ,1] and \
-               close_avg.iloc[i, 0] < close_avg.iloc[i, 1]:
-				singnal_close.iloc[i, 0] = -1
-
-		singnal = pd.DataFrame({'singnal':singnal_volume.singnal+singnal_close.singnal}, \
-								index = singnal_volume.index)
->>>>>>> e30055599ed7bf9ba59fe0e6dc4d69396e135666
-		return singnal
-
-
-	def timing(self, df_volume):
-		volSMA = self.compute_volume(df_volume)
-		close_avg = self.compute_price(df_volume)
-		singnal = self.singnal(volSMA,close_avg)
-<<<<<<< HEAD
-		singnal = singnal.dropna()
-		#print singnal
-		#singnal.to_csv('/home/wangxiaokun/recommend_model/asset_allocation_v1/shell/cc.csv')
-=======
->>>>>>> e30055599ed7bf9ba59fe0e6dc4d69396e135666
-		return singnal
+        volSMA = self.compute_volume(df_nav)
+        close_avg = self.compute_price(df_nav)
+        signal = self.signal(volSMA,close_avg, df_nav)
+        signal.replace(0,np.nan,inplace = True)
+        signal.fillna(method = 'ffill', inplace = True)
+        signal = signal.dropna()
+		
+        return signal
