6d9db2eea32b97632d33ba567652c4ee241fde50
yaojiahui <xzcbvnz@163.com>
2017-10-10 15:41:27 +0800

    fix bug

--- asset_allocation_v1/shell/CommandWavelet.py
+++ asset_allocation_v1/shell/CommandWavelet.py
@@ -7,14 +7,14 @@ import click
 import pandas as pd
 
 from sqlalchemy import MetaData, Table, select
-from TimingWavelet import get_filtered_data
+from TimingWavelet import TimingWt
 from db import asset_wt_filter, base_ra_index_nav, database
 
 
 @click.group(invoke_without_command=True)
 @click.option('--id', 'optid', help=u'filtered index id to update')
 @click.pass_context
-def filtering(ctx, optid, optonline):
+def filtering(ctx, optid):
     '''filtering group
     '''
     if ctx.invoked_subcommand is None:
@@ -27,7 +27,7 @@ def filtering(ctx, optid, optonline):
 @filtering.command()
 @click.option('--id', 'optid', help=u'ids of fund pool to update')
 @click.pass_context
-def nav(ctx, optid, wavenum, startdate, enddate):
+def nav(ctx, optid):
     ''' calc pool nav and inc
     '''
     if optid is not None:
@@ -44,32 +44,34 @@ def nav(ctx, optid, wavenum, startdate, enddate):
 
 def nav_update(filtering):
     ori_data = base_ra_index_nav.load_series(filtering['wt_index_id'])
-    filtered_data = get_filtered_data(ori_data, filtering['wt_filter_num'], \
+    wt = TimingWt(ori_data)
+    filtered_data = wt.get_filtered_data(ori_data, filtering['wt_filter_num'], \
             filtering['wt_begin_date'])
 
-    df_new = database.number_format(filtered_data, columns=['wt_nav', 'wt_inc'], \
+    df_new = database.number_format(filtered_data, wt_nav = 2, wt_inc = 6, \
             precision=2)
     df_new['wt_filter_id'] = filtering['globalid']
-    print df_new
+    df_new = df_new.set_index(['wt_filter_id', 'wt_date'])
 
-'''
     # 加载旧数据
     db = database.connection('asset')
-    t2 = Table('wt_filtering_nav', MetaData(bind=db), autoload=True)
-    columns2 = [
-        t2.c.tc_timing_id,
-        t2.c.tc_date,
-        t2.c.tc_nav,
-        t2.c.tc_inc,
+    t = Table('wt_filter_nav', MetaData(bind=db), autoload=True)
+    columns = [
+        t.c.wt_filter_id,
+        t.c.wt_date,
+        t.c.wt_nav,
+        t.c.wt_inc,
     ]
-    stmt_select = select(columns2, (t2.c.tc_timing_id == filtering['filter_index_id']))
-    df_old = pd.read_sql(stmt_select, db, index_col=['tc_timing_id', 'tc_date'], parse_dates=['tc_date'])
+    stmt_select = select(columns, (t.c.wt_filter_id == filtering['globalid']))
+    df_old = pd.read_sql(stmt_select, db, index_col=['wt_filter_id', 'wt_date'], \
+            parse_dates=['wt_date'])
     if not df_old.empty:
-        df_old = database.number_format(df_old, columns=['tc_nav', 'tc_inc'], precision=2)
+        df_old = database.number_format(df_old, wt_nav = 2, wt_inc = 6)
 
     # 更新数据库
-    database.batch(db, t2, df_new, df_old, timestamp=False)
+    print
+    print df_new.head()
+    database.batch(db, t, df_new, df_old, timestamp=False)
 
     #print df_result.head()
-'''
 
