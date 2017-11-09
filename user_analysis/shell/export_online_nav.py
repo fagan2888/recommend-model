c08db75c4b00c4011a32ef7b718e7164a1aa8517
yjiaoneal <yjiaoneal@163.com>
2017-11-09 13:37:52 +0800

    add code

--- user_analysis/shell/export_online_nav.py
+++ user_analysis/shell/export_online_nav.py
@@ -16,6 +16,16 @@ asset_allocation = {
 }
 
 
+mofang = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "passwd": "Mofang123",
+    "db":"mofang",
+    "charset": "utf8"
+}
+
+
 if __name__ == '__main__':
 
     conn  = MySQLdb.connect(**asset_allocation)
@@ -23,11 +33,10 @@ if __name__ == '__main__':
 
     '''
     sql = 'select on_date as date, on_nav as nav from on_online_nav where on_online_id = 800000 and on_type = 8'
+    sql = 'select on_date as date, on_nav as nav from on_online_nav where on_online_id = 800000 and on_type = 9'
     online_df = pd.read_sql(sql, conn, index_col = ['date'])
     print online_df
     online_df.to_csv('./tmp/online_nav.csv')
-
-
     df = pd.read_csv('data/index.csv', parse_dates = ['date'], index_col = ['date'])
     df = df[['000300.SH']]
     df = pd.concat([online_df, df], axis = 1, join_axes = [online_df.index])
@@ -39,11 +48,18 @@ if __name__ == '__main__':
 
     dfs = []
     for i in range(0, 10):
-        sql = 'select on_date as date, on_nav as nav from on_online_nav where on_online_id = 80000%d and on_type = 9' % i
+        sql = 'select on_date as date, on_nav as nav from on_online_nav where on_online_id = 80000%d and on_type = 8' % i
         df = pd.read_sql(sql, conn, index_col = ['date'], parse_dates = ['date'])
         df.columns = ['risk_' + str(i)]
         dfs.append(df)
 
+    conn  = MySQLdb.connect(**mofang)
+    conn.autocommit(True)
+    sql = 'select ra_date as date, ra_nav as nav from ra_index_nav where ra_index_id = 120000016'
+    df = pd.read_sql(sql, conn, index_col = ['date'], parse_dates = ['date'])
+
+
+    dfs.append(df)
     df = pd.concat(dfs, axis = 1)
     df = df[df.index >= '2016-08-01']
     df = df[df.index <= '2017-10-18']
@@ -76,3 +92,15 @@ if __name__ == '__main__':
     max_drawdown_df = pd.DataFrame(max_drawdowns, index = ds, columns = df.columns)
     #print max_drawdown_df
     max_drawdown_df.to_csv('tmp/max_drawdown.csv')
+
+    #print df
+
+    #df = pd.concat(dfs, axis = 1)
+    #df = df[df.index >= '2016-07-01']
+    #df = df[df.index <= '2017-07-31']
+    #df = df / df.iloc[0]
+    #print df
+
+
+
+    #df.to_csv('./tmp/online_nav.csv')
