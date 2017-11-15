50c55f9a6125f0ab6b59026aa0347c57955968bb
Likun Liu <liulikun@gmail.com>
2017-11-15 11:28:22 +0800

    config bugfixed

--- asset_allocation_v1/shell/db/database.py
+++ asset_allocation_v1/shell/db/database.py
@@ -48,7 +48,7 @@ uris = {
     'asset': config.db_asset_uri,
     'base': config.db_base_uri,
     'caihui': config.db_caihui_uri,
-    #'portfolio_sta': config.db_portfolio_sta_uri,
+    'portfolio_sta': config.db_portfolio_sta_uri,
 }
 
 connections = {}
