fb95001e7fe886361daf99b124265b06192be163
yjiaoneal <yjiaoneal@163.com>
2018-01-14 21:51:02 +0800

    add code

--- asset_allocation_v1/shell/CommandStock.py
+++ asset_allocation_v1/shell/CommandStock.py
@@ -19,6 +19,7 @@ import util_numpy as npu
 import Portfolio as PF
 from TimingWavelet import TimingWt
 import multiprocessing
+from multiprocessing import Pool
 from multiprocessing import Manager
 
 from datetime import datetime, timedelta
@@ -35,6 +36,7 @@ from mxnet import nd
 from db.asset_stock import *
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
+import functools
 
 import traceback, code
 
@@ -132,25 +134,96 @@ def all_stock_info():
     return all_stocks
 
 
+
+def compute_stock_wavelet(last_date, secode):
+
+    engine = database.connection('caihui')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    sql = session.query(tq_sk_dquoteindic.tradedate, tq_sk_dquoteindic.tcloseaf).filter(tq_sk_dquoteindic.secode == secode).filter(tq_sk_dquoteindic.tradedate <= last_date).statement
+
+    quotation = pd.read_sql(sql, session.bind , index_col = ['tradedate'], parse_dates = ['tradedate'])
+    if len(quotation) == 0:
+        return secode, -100.0
+    sr = quotation.tcloseaf
+    dates = pd.date_range(sr.index[0], sr.index[-1])
+    sr = sr.loc[dates].fillna(method = 'pad')
+    '''
+    wt = TimingWt(sr)
+    try:
+        filtered_data = wt.wavefilter(sr, 3)
+    except:
+        return secode, -100.0
+    '''
+    sr = sr.iloc[-90:,]
+    #sr = filtered_data.iloc[-181:,]
+    sr = sr.pct_change().fillna(0.0)
+    #print secode, dates[-1], sr.mean() / sr.std()
+    return secode, sr.mean() / ( sr.std() * 5 )
+
+    session.commit()
+    session.close()
+
+
 @stock.command()
 @click.pass_context
 def stock_wavelet(ctx):
 
     all_stocks = all_stock_info()
+    all_stocks = all_stocks.iloc[0:1000]
 
-    engine = database.connection('caihui')
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for i in range(0, len(all_stocks.index)):
-        secode = all_stocks.index[i]
-        globalid = all_stocks.loc[secode, 'globalid']
-        sql = session.query(tq_sk_dquoteindic.tradedate, tq_sk_dquoteindic.tcloseaf).filter(tq_sk_dquoteindic.secode == secode).statement
-        quotation = pd.read_sql(sql, session.bind , index_col = ['tradedate'], parse_dates = ['tradedate'])
-        sr = quotation.tcloseaf
-        wt = TimingWt(sr)
-        filtered_data = wt.wavefilter(sr, 2)
-        filtered_data = filtered_data.fillna(0.0)
-        print filtered_data
+    dates = pd.date_range('2017-01-15', '2018-01-10')
+    dates = list(dates)
+    dates = dates[::30]
 
 
+    stock_pos = {}
+    for last_date in dates:
 
+        pool = Pool(20)
+        result = pool.map(functools.partial(compute_stock_wavelet, last_date.strftime('%Y%m%d')), all_stocks.index)
+        pool.close()
+        pool.join()
+
+        sorted_sharp = sorted(result, lambda x, y: cmp(x[1], y[1]), reverse = True)
+        records = stock_pos.setdefault(last_date, [])
+        #sorted_sharp = sorted(result, lambda x, y: cmp(x[1], y[1]))
+        for secode, sharp in sorted_sharp[0:10]:
+            print all_stocks.loc[secode, 'globalid'], all_stocks.loc[secode, 'sk_name'], last_date ,sharp
+            records.append(secode)
+
+
+    globalid_secode_dict = dict(zip(all_stocks.index.ravel(), all_stocks.globalid.ravel()))
+    #print stock_pos
+    dates = list(stock_pos.keys())
+    dates.sort()
+    stock_pos_df = pd.DataFrame(0, index = dates, columns = all_stocks.index)
+    for date in dates:
+        stocks = stock_pos[date]
+        record = stock_pos_df.loc[date]
+        record[record.index.isin(stocks)] = 1.0 / len(stocks)
+        stock_pos_df.loc[date] = record
+    #stock_pos_df = stock_pos_df.rename(columns = globalid_secode_dict)
+
+    caihui_engine = database.connection('caihui')
+    caihui_Session = sessionmaker(bind=caihui_engine)
+    caihui_session = caihui_Session()
+
+    sql = caihui_session.query(tq_sk_yieldindic.tradedate, tq_sk_yieldindic.secode ,tq_sk_yieldindic.Yield).filter(tq_sk_yieldindic.tradedate >= stock_pos_df.index[0].strftime('%Y%m%d')).statement
+    stock_yield_df = pd.read_sql(sql, caihui_session.bind, index_col = ['tradedate', 'secode'], parse_dates = ['tradedate']) / 100.0
+    stock_yield_df = stock_yield_df.unstack()
+    stock_yield_df.columns = stock_yield_df.columns.droplevel(0)
+    secodes = list(set(stock_pos_df.columns) & set(stock_yield_df.columns))
+    stock_pos_df = stock_pos_df[secodes]
+    stock_yield_df = stock_yield_df[secodes]
+    caihui_session.commit()
+    caihui_session.close()
+
+    stock_pos_df = stock_pos_df.reindex(stock_yield_df.index).fillna(method = 'pad')
+    stock_pos_df = stock_pos_df.shift(1).fillna(0.0)
+
+    factor_yield_df = stock_pos_df * stock_yield_df
+    factor_yield_df = factor_yield_df.sum(axis = 1)
+    factor_nav_df = (1 + factor_yield_df).cumprod()
+
+    print factor_nav_df.index[-1], factor_nav_df.iloc[-1]
