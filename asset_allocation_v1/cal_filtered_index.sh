af5335a7bd84fd88e66e8e9bf980ffe6d78bc551
yaojiahui <xzcbvnz@163.com>
2018-02-03 11:40:39 +0800

    add cal_filtered_index

---
+++ asset_allocation_v1/cal_filtered_index.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python shell/roboadvisor.py markowitz --id MZ.FT0010 --new
+python shell/roboadvisor.py markowitz --id MZ.FT0020 --new
+python shell/roboadvisor.py markowitz --id MZ.FT0030 --new
+python shell/roboadvisor.py markowitz --id MZ.FT0040 --new
+python shell/roboadvisor.py markowitz --id MZ.FT0050 --new
