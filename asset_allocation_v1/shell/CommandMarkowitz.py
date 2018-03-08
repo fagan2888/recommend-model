cd267ecd0deba0645e96d9525a4237b904b991fe
yaojiahui <xzcbvnz@163.com>
2018-03-08 14:58:42 +0800

    add CalCayle.py to calculate the cycles of sumple assets

--- asset_allocation_v1/shell/CommandMarkowitz.py
+++ asset_allocation_v1/shell/CommandMarkowitz.py
@@ -28,11 +28,14 @@ from sqlalchemy import MetaData, Table, select, func
 from sqlalchemy.orm import sessionmaker
 from tabulate import tabulate
 from db import database, asset_mz_markowitz, asset_mz_markowitz_alloc, asset_mz_markowitz_argv,  asset_mz_markowitz_asset, asset_mz_markowitz_criteria, asset_mz_markowitz_nav, asset_mz_markowitz_pos, asset_mz_markowitz_sharpe, asset_wt_filter_nav
-from db import asset_ra_pool, asset_ra_pool_nav, asset_rs_reshape, asset_rs_reshape_nav, asset_rs_reshape_pos,asset_stock,asset_stock_factor
+from db import asset_ra_pool, asset_ra_pool_nav, asset_rs_reshape, asset_rs_reshape_nav, asset_rs_reshape_pos,asset_stock,asset_stock_factor, asset_fl_nav
 from db import base_ra_index, base_ra_index_nav, base_ra_fund, base_ra_fund_nav, base_trade_dates, base_exchange_rate_index_nav
 from util import xdict
 from util.xdebug import dd
 import stock_factor ,barra_stock_factor, stock_factor_util, corr_regression_tree
+from util import xdict
+from util.xdebug import dd
+from ipdb import set_trace
 
 import traceback, code
 
@@ -667,6 +670,10 @@ def load_wavelet_nav_series(asset_id, reindex=None, begin_date=None, end_date=No
         sr = base_exchange_rate_index_nav.load_series(
             asset_id, reindex=None, end_date=end_date)
 
+    elif xtype == 'FL.':
+
+        sr = asset_fl_nav.load_series(
+            asset_id, reindex=None, end_date=end_date)
     else:
         sr = pd.Series()
 
@@ -766,6 +773,9 @@ def load_nav_series(asset_id, reindex=None, begin_date=None, end_date=None):
             # markowitz配置资产
             #
             sr = asset_stock_factor.load_factor_nav_series(
+        elif prefix == 'BL':
+
+            sr = asset_fl_nav.load_series(
                 asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
         else:
             sr = pd.Series()
--- asset_allocation_v1/shell/db/asset_fl_nav.py
+++ asset_allocation_v1/shell/db/asset_fl_nav.py
@@ -29,3 +29,32 @@ def load_nav(id_):
     df = pd.read_sql(s, db, index_col=['fl_id', 'fl_date'], parse_dates = ['fl_date'])
 
     return df
+
+def load_series(id_, reindex=None, begin_date=None, end_date=None, mask=None):
+    db = database.connection('asset')
+    metadata = MetaData(bind=db)
+    t1 = Table('fl_nav', metadata, autoload=True)
+
+    columns = [
+        t1.c.fl_date.label('date'),
+        t1.c.fl_nav.label('nav'),
+    ]
+
+    s = select(columns).where(t1.c.fl_id == id_)
+    
+    if begin_date is not None:
+        s = s.where(t1.c.fl_date >= begin_date)
+    if end_date is not None:
+        s = s.where(t1.c.fl_date <= end_date)
+    if mask is not None:
+        if hasattr(mask, "__iter__") and not isinstance(mask, str):
+            s = s.where(t1.c.fl_mask.in_(mask))
+        else:
+            s = s.where(t1.c.fl_mask == mask)
+        
+    df = pd.read_sql(s, db, index_col = ['date'], parse_dates=['date'])
+
+    if reindex is not None:
+        df = df.reindex(reindex, method='pad')
+
+    return df['nav']
