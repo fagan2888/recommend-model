2c644f020e99c9b09ad97db3b66063fcae262ff0
Raycursive <m@raycursive.com>
2018-03-21 16:02:59 +0800

    just some modification

--- asset_allocation_v1/shell/Portfolio.py
+++ asset_allocation_v1/shell/Portfolio.py
@@ -197,7 +197,7 @@ def markowitz_r(funddfr, bounds):
     return final_risk, final_return, final_ws, final_sharp
 
 
-def markowitz_r_spe(funddfr, delta, weights, P, eta, alpha, bounds, risk_parity):
+def markowitz_r_spe(funddfr, bounds):
 
     rf = Const.rf
 
@@ -208,7 +208,7 @@ def markowitz_r_spe(funddfr, delta, weights, P, eta, alpha, bounds, risk_parity)
     final_codes = []
 
     
-    risks, returns, ws = fin.efficient_frontier_spe(funddfr, delta, weights, P, eta, alpha, bounds, risk_parity)
+    risks, returns, ws = fin.efficient_frontier_spe(funddfr, bounds)
 
     for j in range(0, len(risks)):
         sharp = (returns[j] - rf) / risks[j]
@@ -220,14 +220,46 @@ def markowitz_r_spe(funddfr, delta, weights, P, eta, alpha, bounds, risk_parity)
 
     return final_risk, final_return, final_ws, final_sharp
 
-def m_markowitz(queue, random_index, df_inc, delta, weights, P, eta, alpha, bound, risk_parity):
+
+def markowitz_r_spe_bl(funddfr, P, eta, alpha, bounds):
+
+    rf = Const.rf
+
+    final_risk = 0
+    final_return = 0
+    final_ws = []
+    final_sharp = -10000000000000000000000000.0
+    final_codes = []
+
+    
+    risks, returns, ws = fin.efficient_frontier_spe_bl(funddfr, P, eta, alpha, bounds)
+
+    for j in range(0, len(risks)):
+        sharp = (returns[j] - rf) / risks[j]
+        if sharp > final_sharp:
+            final_risk = risks[j]
+            final_return = returns[j]
+            final_ws = ws[j]
+            final_sharp = sharp
+
+    return final_risk, final_return, final_ws, final_sharp
+
+    
+def m_markowitz(queue, random_index, df_inc, bound):
     for index in random_index:
         tmp_df_inc = df_inc.iloc[index]
-        risk, returns, ws, sharpe = markowitz_r_spe(tmp_df_inc, delta, weights, P, eta, alpha, bound, risk_parity)
+        risk, returns, ws, sharpe = markowitz_r_spe(tmp_df_inc, bound)
         queue.put((risk, returns, ws, sharpe))
 
 
-def markowitz_bootstrape(df_inc, delta, weights, P, eta, alpha, bound, risk_parity, cpu_count = 0, bootstrap_count=0):
+def m_markowitz_bl(queue, random_index, df_inc, P, eta, alpha, bound):
+    for index in random_index:
+        tmp_df_inc = df_inc.iloc[index]
+        risk, returns, ws, sharpe = markowitz_r_spe_bl(tmp_df_inc, P, eta, alpha, bound)
+        queue.put((risk, returns, ws, sharpe))
+
+
+def markowitz_bootstrape_bl(df_inc, P, eta, alpha, bound, cpu_count = 0, bootstrap_count=0):
 
     os.environ['OMP_NUM_THREADS'] = '1'
 
@@ -266,7 +298,71 @@ def markowitz_bootstrape(df_inc, delta, weights, P, eta, alpha, bound, risk_pari
     q = manager.Queue()
     processes = []
     for indexs in process_indexs:
-        p = multiprocessing.Process(target = m_markowitz, args = (q, indexs, df_inc, delta, weights, P, eta, alpha, bound, risk_parity))
+        p = multiprocessing.Process(target = m_markowitz_bl, args = (q, indexs, df_inc, P, eta, alpha, bound))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    wss = np.zeros(len(df_inc.columns))
+    risks = []
+    returns = []
+    sharpes = []
+    for m in range(0, q.qsize()):
+        record = q.get(m)
+        ws = record[2]
+        for n in range(0, len(ws)):
+            w = ws[n]
+            wss[n] = wss[n] + w
+        risks.append(record[0])
+        returns.append(record[1])
+        sharpes.append(record[3])
+
+
+    ws = wss / loop_num
+    return np.mean(risks), np.mean(returns), ws, np.mean(sharpes)
+
+def markowitz_bootstrape(df_inc, bound, cpu_count = 0, bootstrap_count=0):
+
+    os.environ['OMP_NUM_THREADS'] = '1'
+
+    if cpu_count == 0:
+        count = multiprocessing.cpu_count()
+        cpu_count = count if count > 0 else 1
+
+    look_back = len(df_inc)
+    if bootstrap_count <= 0:
+        loop_num = look_back * 4
+    elif bootstrap_count % 2:
+        loop_num = bootstrap_count + 1
+    else:
+        loop_num = bootstrap_count
+
+    # logger.info("bootstrap_count: %d, cpu_count: %d", loop_num, cpu_count)
+    process_indexs = [[] for i in range(0, cpu_count)]
+
+    #print process_indexs
+    #loop_num = 20
+    rep_num = loop_num * (look_back / 2) / look_back
+    day_indexs = range(0, look_back) * rep_num
+    random.shuffle(day_indexs)
+    #print day_indexs
+    day_indexs = np.array(day_indexs)
+
+
+    day_indexs = day_indexs.reshape(len(day_indexs) / (look_back / 2), look_back / 2)
+    for m in range(0, len(day_indexs)):
+        indexs = day_indexs[m]
+        mod = m % cpu_count
+        process_indexs[mod].append(list(indexs))
+
+
+    manager = Manager()
+    q = manager.Queue()
+    processes = []
+    for indexs in process_indexs:
+        p = multiprocessing.Process(target = m_markowitz, args = (q, indexs, df_inc, bound))
         processes.append(p)
         p.start()
 
