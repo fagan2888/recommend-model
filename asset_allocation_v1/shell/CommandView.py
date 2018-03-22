30b45485469e93d39715d2e2573d47e3cfe78b75
yaojiahui <xzcbvnz@163.com>
2018-03-22 15:14:02 +0800

    fix CommandView.py

--- asset_allocation_v1/shell/CommandView.py
+++ asset_allocation_v1/shell/CommandView.py
@@ -27,6 +27,7 @@ from db import *
 from db import asset_ra_bl, asset_ra_bl_argv, asset_ra_bl_asset, asset_ra_bl_view
 from CommandMarkowitz import load_wavelet_nav_series, load_nav_series
 from util.xdebug import dd
+from heapq import nlargest
 
 import traceback, code
 from ipdb import set_trace
@@ -85,7 +86,65 @@ def signal_update_wavelet(view):
             bar.update(1)
 
     df_new = pd.concat(asset_views)
+
+    dates = df_new.index.unique()
+    df_new = df_new.astype('object')
+    for date in dates:
+        ## asset with max sharpe ratio get view 1
+        # df_new.loc[date, 'bl_view'] = np.sign(df_new.loc[date, 'bl_view'] - df_new.loc[date, 'bl_view'].max()) + 1
+        df_new.loc[date, 'bl_view'] = convert_sharpe_to_view_2(df_new.loc[date, 'bl_view'].values.ravel())
+
+        ## asset with max sharpe ratio get view 1, asset with min sharpe ratio get view -1
+        # df_new.loc[date, 'bl_view'] = convert_sharpe_to_view(df_new.loc[date, 'bl_view'].values.ravel())
+
+        ## asset with max sharpe ratio get view 2, asset with second largest sharpe ratio get view 1
+        # df_new.loc[date, 'bl_view'] = convert_sharpe_to_view_3(df_new.loc[date, 'bl_view'].values.ravel())
+
+    df_new = df_new.reset_index()
+    df_new = df_new.set_index(['bl_date', 'globalid', 'bl_index_id'])
+
     asset_ra_bl_view.save(viewid, df_new)
+
+
+def convert_sharpe_to_view(x):
+    x = x.astype(float)
+    Min = min(x)
+    Max = max(x)
+    for i in range(len(x)):
+        if x[i] == Min:
+            x[i] = -1
+        elif x[i] == Max:
+            x[i] = 1
+        else:
+            x[i] = 0
+
+    return x
+
+
+def convert_sharpe_to_view_2(x):
+    x = x.astype(float)
+    Max = max(x)
+    for i in range(len(x)):
+        if x[i] == Max:
+            x[i] = 2
+        else:
+            x[i] = 0
+
+    return x
+
+
+def convert_sharpe_to_view_3(x):
+    x = x.astype(float)
+    Max, Smax = nlargest(2, x)
+    for i in range(len(x)):
+        if x[i] == Max:
+            x[i] = 2
+        elif x[i] == Smax:
+            x[i] = 1
+        else:
+            x[i] = 0
+
+    return x
 
 
 def cal_wavelet_view(view, indexid, wavenum, trend_lookback, start_date):
@@ -96,35 +155,29 @@ def cal_wavelet_view(view, indexid, wavenum, trend_lookback, start_date):
         wavelet_nav_series = load_wavelet_nav_series(indexid, end_date = date, wavelet_filter_num = int(wavenum))
         view_nav_series = wavelet_nav_series.loc[lookback_date].dropna()
 
-        nav_series = load_nav_series(indexid, end_date = date)
-        nav_series = nav_series.loc[view_nav_series.index]
+        ## view ver 1
+        # bl_view = np.sign(view_nav_series.iloc[-1]-view_nav_series.iloc[0])
 
-        view_nav_series = view_nav_series / view_nav_series.iloc[0]
-        nav_series = nav_series / nav_series.iloc[0]
-        #bl_view = np.sign(view_nav_series.iloc[-1]-view_nav_series.iloc[0])
+        ## view ver 2
+        # nav_series = load_nav_series(indexid, end_date = date)
+        # nav_series = nav_series.loc[view_nav_series.index]
+        # view_nav_series = view_nav_series / view_nav_series.iloc[0]
+        # nav_series = nav_series / nav_series.iloc[0]
+        # bl_view = np.sign(view_nav_series.iloc[-1] - nav_series.iloc[-1])
+        wave_ret = (view_nav_series.pct_change().dropna()).mean()
+        wave_std = (view_nav_series.pct_change().dropna()).std()
+        wave_shape = (wave_ret - Const.rf)/wave_std
+        bl_view = wave_shape
 
-        #print view_nav_series.tail(), nav_series.tail()
-        #print
-        bl_view = np.sign(view_nav_series.iloc[-1] - nav_series.iloc[-1])
         bl_views.append(bl_view)
 
     df = pd.DataFrame(bl_views, index = trade_dates, columns = ['bl_view'])
     df.index.name = 'bl_date'
     df['globalid'] = view['globalid']
     df['bl_index_id'] = indexid
-    df = df.reset_index()
-    df = df.set_index(['bl_date', 'globalid', 'bl_index_id'])
+    # df = df.reset_index()
+    # df = df.set_index(['bl_date', 'globalid', 'bl_index_id'])
     return df
-
-
-
-
-
-
-
-
-
-
 
 
 
