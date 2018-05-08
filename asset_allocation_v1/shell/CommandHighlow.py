ce0a35d4b65cfe128f34ec4a3e9db8ae64f9e5f1
yjiaoneal <yjiaoneal@163.com>
2018-05-08 14:57:46 +0800

    添加高低风险微调机制

--- asset_allocation_v1/shell/CommandHighlow.py
+++ asset_allocation_v1/shell/CommandHighlow.py
@@ -23,17 +23,21 @@ from dateutil.parser import parse
 from Const import datapath
 from sqlalchemy import MetaData, Table, select, func
 from tabulate import tabulate
+from db import asset_mz_highlow_limit
 from db import *
 from util import xdict
 from util.xdebug import dd
+from RiskParity import cal_weight
 
 import traceback, code
 
+
 logger = logging.getLogger(__name__)
 
-@click.group(invoke_without_command=True)  
+@click.group(invoke_without_command=True)
 @click.option('--full/--no-full', 'optfull', default=False, help=u'include all instance')
 @click.option('--new/--no-new', 'optnew', default=False, help=u'use new framework')
+@click.option('--limit/--no-limit', 'optlimit', default=False, help=u'calculate time varying limit')
 @click.option('--id', 'optid', help=u'specify markowitz id')
 @click.option('--name', 'optname', default=None, help=u'specify highlow name')
 @click.option('--type', 'opttype', type=click.Choice(['1', '9']), default='1', help=u'online type(1:expriment; 9:online)')
@@ -44,14 +48,17 @@ logger = logging.getLogger(__name__)
 @click.option('--riskmgr/--no-riskmgr', 'optriskmgr', default=True, help=u'with riskmgr or not')
 @click.option('--risk', 'optrisk', default='10,1,2,3,4,5,6,7,8,9', help=u'which risk to calc, [1-10]')
 @click.option('--end-date', 'optenddate', default=None, help=u'calc end date for nav')
+@click.option('--start-date', 'optstartdate', default='2012-07-06', help=u'start date')
 @click.pass_context
-def highlow(ctx, optfull, optnew, optid, optname, opttype, optreplace, opthigh, optlow, optriskmgr, optrisk, optenddate):
+def highlow(ctx, optfull, optnew, optlimit, optid, optname, opttype, optreplace, opthigh, optlow, optriskmgr, optrisk, optenddate, optstartdate):
 
     '''markowitz group
     '''
     if ctx.invoked_subcommand is None:
         # click.echo('I was invoked without subcommand')
         if optnew:
+            if optlimit:
+                ctx.invoke(limit, optid=optid, optrisk=optrisk, optstartdate=optstartdate)
             ctx.invoke(pos, optid=optid, optrisk=optrisk)
             ctx.invoke(nav, optid=optid, optenddate=optenddate)
             ctx.invoke(turnover, optid=optid)
@@ -176,7 +183,7 @@ def allocate(ctx, optid, optname, opttype, optreplace, opthigh, optlow, optriskm
         df_asset.loc['11310100'] = (0, u'货币(低)', 31, 0, '11310100')
     if '11310101' not in df_asset.index:
         df_asset.loc['11310101'] = (0, u'货币(高)', 31, 0, '11310101')
-    
+
     db = database.connection('asset')
     metadata = MetaData(bind=db)
     mz_highlow        = Table('mz_highlow', metadata, autoload=True)
@@ -390,48 +397,106 @@ def load_riskmgr2(assets, sr_riskmgr, reindex=None, enable=True):
 def load_nav_series(asset_id, reindex=None, begin_date=None, end_date=None):
 
 
-    if asset_id.isdigit():
+    prefix = asset_id[0:2]
+    if prefix.isdigit():
         xtype = int(asset_id) / 10000000
+        if xtype == 1:
+            #
+            # 基金池资产
+            #
+            asset_id = int(asset_id) % 10000000
+            (pool_id, category) = (asset_id / 100, asset_id % 100)
+            ttype = pool_id / 10000
+            sr = asset_ra_pool_nav.load_series(
+                pool_id, category, ttype, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif xtype == 3:
+            #
+            # 基金池资产
+            #
+            sr = base_ra_fund_nav.load_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif xtype == 4:
+            #
+            # 修型资产
+            #
+            sr = asset_rs_reshape_nav.load_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif xtype == 12:
+            #
+            # 指数资产
+            #
+            sr = base_ra_index_nav.load_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif xtype == 'ERI':
+
+            sr = base_exchange_rate_index_nav.load_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        else:
+            sr = pd.Series()
     else:
-        xtype = re.sub(r'([\d]+)','',asset_id).strip()
+        if prefix == 'AP':
+            #
+            # 基金池资产
+            #
+            sr = asset_ra_pool_nav.load_series(
+                asset_id, 0, 9, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif prefix == 'FD':
+            #
+            # 基金资产
+            #
+            sr = base_ra_fund_nav.load_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif prefix == 'RS':
+            #
+            # 修型资产
+            #
+            sr = asset_rs_reshape_nav.load_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif prefix == 'IX':
+            #
+            # 指数资产
+            #
+            sr = base_ra_index_nav.load_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif prefix == 'ER':
+            #
+            # 人民币计价的指数资产
+            #
+            sr = base_exchange_rate_index_nav.load_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif prefix == 'SK':
+            #
+            # 股票资产
+            #
+            sr = asset_stock.load_stock_nav_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif prefix == 'MZ':
+            #
+            # markowitz配置资产
+            #
+            sr = asset_mz_markowitz_nav.load_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+        elif prefix == 'BF':
+            #
+            # markowitz配置资产
+            #
+            sr = asset_stock_factor.load_factor_nav_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+
+        elif prefix == 'FC':
+
+            # sr = asset_factor_cluster.load_series(
+            #     asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
+            sr = asset_factor_cluster.load_selected_factor_series(
+                asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
 
 
-    if xtype == 1:
-        #
-        # 基金池资产
-        #
-        asset_id = string.atoi(asset_id) / 10000000
-        (pool_id, category) = (asset_id / 100, asset_id % 100)
-        ttype = pool_id / 10000
-        sr = asset_ra_pool_nav.load_series(
-            pool_id, category, ttype, reindex=reindex, begin_date=begin_date, end_date=end_date)
-    elif xtype == 3:
-        #
-        # 基金池资产
-        #
-        sr = base_ra_fund_nav.load_series(
-            asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
-    elif xtype == 4:
-        #
-        # 修型资产
-        #
-        sr = asset_rs_reshape_nav.load_series(
-            asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
-    elif xtype == 12:
-        #
-        # 指数资产
-        #
-        sr = base_ra_index_nav.load_series(
-            asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
-    elif xtype == 'ERI':
-
-        sr = base_exchange_rate_index_nav.load_series(
-            asset_id, reindex=reindex, begin_date=begin_date, end_date=end_date)
-
-    else:
-        sr = pd.Series()
+        else:
+            sr = pd.Series()
 
     return sr
+
+
 
 @highlow.command()
 @click.option('--id', 'optid', help=u'ids of highlow to update')
@@ -456,7 +521,7 @@ def pos(ctx, optid, opttype, optlist, optrisk):
         df_highlow = asset_mz_highlow.load(highlows)
     else:
         df_highlow = asset_mz_highlow.load(highlows, xtypes)
-        
+
     if optlist:
         df_highlow['mz_name'] = df_highlow['mz_name'].map(lambda e: e.decode('utf-8'))
         print tabulate(df_highlow, headers='keys', tablefmt='psql')
@@ -504,7 +569,7 @@ def pos_update(highlow, alloc):
     # algo = alloc['mz_algo'] if alloc['mz_algo'] != 0 else markowitz['mz_algo']
     algo = highlow['mz_algo']
 
-    if algo == 1:
+    if algo == 1 or algo ==3:
         #
         # 焦氏策略
         #
@@ -523,7 +588,15 @@ def pos_update(highlow, alloc):
             highlow['mz_high_id'] = df_markowitz_alloc.at[max_ix, 'globalid']
             highlow['mz_low_id'] = df_markowitz_alloc.at[min_ix, 'globalid']
 
-        df = jiao(highlow, alloc)
+        if algo == 1:
+            df = jiao(highlow, alloc)
+
+        elif algo == 3:
+            #df_highlow = asset_mz_highlow.load(highlow)
+            ratio_hl = cal_limit(highlow)
+            ratio_hl = limit_update(highlow, alloc, ratio_hl)
+            df = jiao2(highlow, alloc, ratio_hl)
+
     elif algo == 2:
         #
         # 姚氏策略
@@ -532,7 +605,6 @@ def pos_update(highlow, alloc):
     else:
         click.echo(click.style("\n unknow algo %d for %s\n" % (algo, highlow_id), fg='red'))
         return
-
 
 
     #
@@ -560,8 +632,6 @@ def pos_update(highlow, alloc):
     df_tosave = df.stack().to_frame('mz_ratio')
     df_tosave = df_tosave.loc[(df_tosave['mz_ratio'] > 0)]
 
-
-
     #print alloc
     #print df_tosave[df_tosave.index.duplicated()]
     #if alloc['mz_risk'] == 0.9:
@@ -570,6 +640,115 @@ def pos_update(highlow, alloc):
     # save
     # print df_tosave
     asset_mz_highlow_pos.save(highlow_id, df_tosave)
+
+
+def range_cons(x):
+    if x > 1:
+        return 1
+    elif x < 0:
+        return 0
+    else:
+        return x
+
+
+def limit_update(highlow, alloc, ratio_hl):
+    # ratio_hl = cal_limit(highlow, alloc)
+    risk = alloc.mz_risk
+
+    # ratio_hl = ratio_hl / 5
+    # ratio_hl['ratio_h'] += ((risk - 0.1)/0.9 - 0.1)
+
+    ratio_hl = ratio_hl / 2.5
+    ratio_hl['ratio_h'] += ((risk - 0.1)/0.9 - 0.2)
+
+    ratio_hl['ratio_h'] = ratio_hl['ratio_h'].apply(range_cons)
+    ratio_hl['ratio_l'] = 1.0 - ratio_hl['ratio_h']
+    ratio_hl.index.name = 'mz_date'
+    #ratio_hl = ratio_hl.reset_index()
+    #ratio_hl['mz_risk'] = risk
+    #ratio_hl['mz_highlow_id'] = alloc.mz_highlow_id
+    #ratio_hl['globalid'] = alloc.globalid
+    #ratio_hl = ratio_hl.set_index(['globalid', 'mz_highlow_id', 'mz_risk', 'mz_date'])
+
+    #asset_mz_highlow_limit.save(alloc.mz_highlow_id, risk, ratio_hl)
+
+    return ratio_hl
+
+def cal_limit(highlow):
+    lookback = 26
+    large_number = 1e4
+    high = highlow['mz_high_id']
+    low = highlow['mz_low_id']
+    high_budget = 0.95
+    risk_budget = [high_budget, 1.0 - high_budget]
+
+    # risk = int(alloc['mz_risk'] * 10)
+    # highlow_range_bound = {
+    #     1 :[0.0, 0.1],
+    #     2 :[0.1, 0.2],
+    #     3 :[0.2, 0.3],
+    #     4 :[0.3, 0.4],
+    #     5 :[0.4, 0.5],
+    #     6 :[0.5, 0.6],
+    #     7 :[0.6, 0.7],
+    #     8 :[0.7, 0.8],
+    #     9 :[0.8, 0.9],
+    #     10:[0.9, 1.0],
+    # }
+
+    # hr = [0.0, 1.0]
+    # bound = {
+    #     'high': {'sum1': 0, 'sum2' : 0, 'upper': hr[1], 'lower': hr[0]},
+    #     'low':  {'sum1': 0, 'sum2' : 0, 'upper': hr[1], 'lower': hr[0]},
+    # }
+    # # risk_budget = [(risk-1)/9.0, (10-risk)/9.0]
+
+    high_nav = asset_mz_markowitz_nav.load_series(high)
+    low_nav = asset_mz_markowitz_nav.load_series(low)
+
+    start_date = high_nav.index[0] if high_nav.index[0] > low_nav.index[0] else low_nav.index[0]
+
+    trade_dates = DBData.trade_date_index(start_date = '2009-01-01')
+    # test_dates = DBData.trade_date_index(start_date = '2012-07-06')
+    test_dates = DBData.trade_date_index(start_date = start_date)[lookback:]
+    # test_dates = DBData.trade_date_index(start_date = '2018-01-02')
+
+    high_nav = asset_mz_markowitz_nav.load_series(high)
+    low_nav = asset_mz_markowitz_nav.load_series(low)
+
+    high_nav = high_nav.reindex(trade_dates).dropna()
+    low_nav = low_nav.reindex(trade_dates).dropna()
+
+    # f = open('tmp/hl_pos_3/hl_pos_risk_{}.csv'.format(risk), 'wb')
+    # f.write('date, high_pos_{0}, low_pos_{0}\n'.format(risk))
+    ratio_hs = []
+    ratio_ls = []
+    for date in test_dates:
+        lookback_dates = DBData.trade_date_lookback_index(end_date = date, lookback = lookback)
+        tmp_high_nav = high_nav.loc[lookback_dates]
+        tmp_low_nav = low_nav.loc[lookback_dates]
+        tmp_df = pd.concat([tmp_high_nav, tmp_low_nav], 1)
+        tmp_cov = (tmp_df.pct_change().dropna()).cov().values
+        hl_pos = cal_weight(tmp_cov*large_number, risk_budget)
+        ratio_h, ratio_l = hl_pos
+        # tmp_df.columns = ['high', 'low']
+        # tmp_df = tmp_df.fillna(method = 'pad')
+        # hl_pos = markowitz_r(tmp_df, today = None, limits = bound, bootstrap = 0, cpu_count = 36, blacklitterman = False, markowitz_id = None)
+        # ratio_h, ratio_l = hl_pos.values[:2]
+        ratio_hs.append(ratio_h)
+        ratio_ls.append(ratio_l)
+        # f.write('{}, {}, {}\n'.format(date, ratio_h, ratio_l))
+    # f.close()
+    df = pd.DataFrame(
+        data = np.column_stack([ratio_hs, ratio_ls]),
+        index = test_dates,
+        columns = ['ratio_h', 'ratio_l'],
+        )
+
+    #df = df.rolling(4).mean().dropna()
+
+    return df
+
 
 def jiao(highlow, alloc):
 
@@ -643,6 +822,88 @@ def jiao(highlow, alloc):
     df = df.groupby(df.index).sum()
     df = df.T
     return df
+
+
+def jiao2(highlow, alloc, ratio_hl):
+
+    high = highlow['mz_high_id']
+    low  = highlow['mz_low_id']
+    risk = alloc['mz_risk']
+    mz_highlow_id = alloc['mz_highlow_id']
+    # argv = asset_mz_highlow_argv.load([mz_highlow_id])
+    # highlow_limit_id = argv.loc['HL.JY1350'].loc['highlow_limit_id']['mz_value']
+
+    df_asset = asset_mz_highlow_asset.load([highlow['globalid']])
+    df_asset.set_index(['mz_asset_id'], inplace=True)
+
+    #
+    # 加载高风险资产仓位
+    #
+    index = None
+
+    df_high = asset_mz_markowitz_pos.load_raw(high)
+    df_high_riskmgr = load_riskmgr2(df_high.columns, df_asset['mz_riskmgr_id'], df_high.index, True)
+    index = df_high.index.union(df_high_riskmgr.index)
+
+    #
+    # 加载低风险资产仓位
+    #
+    df_low  = asset_mz_markowitz_pos.load_raw(low)
+    df_low_riskmgr = load_riskmgr2(df_low.columns, df_asset['mz_riskmgr_id'], df_low.index, True)
+    if index is None:
+        index = df_low.index.union(df_low_riskmgr.index)
+    else:
+        index = index.union(df_low.index).union(df_low_riskmgr.index)
+
+    # 配置比例
+    # ratio_hl = asset_mz_highlow_limit.load_series(mz_highlow_id, risk)
+
+    data_h = {}
+    if not df_high.empty:
+        df_high = df_high.reindex(index, method='pad')
+        df_high_riskmgr = df_high_riskmgr.reindex(index, method='pad')
+        ratio_h = ratio_hl.reindex(index).fillna(method = 'ffill').fillna((risk - 0.1) / 0.9).ratio_h.rolling(4, min_periods = 1).mean()
+        df_high = df_high.loc[ratio_h.index[0]:]
+        df_high_riskmgr = df_high_riskmgr.loc[ratio_h.index[0]:]
+        for column in df_high.columns:
+            data_h[column] = df_high[column] * df_high_riskmgr[column] * ratio_h
+    df_h = pd.DataFrame(data_h)
+
+    data_l = {}
+    if not df_low.empty:
+        df_low = df_low.reindex(index, method='pad')
+        df_low_riskmgr = df_low_riskmgr.reindex(index, method='pad')
+        ratio_l = ratio_hl.reindex(index).fillna(method = 'ffill').fillna(1.0 - (risk - 0.1) / 0.9).ratio_l.rolling(4, min_periods = 1).mean()
+        df_low = df_low[ratio_l.index[0]:]
+        df_low_riskmgr = df_low_riskmgr.loc[ratio_l.index[0]:]
+        for column in df_low.columns:
+            data_l[column] = df_low[column] * df_low_riskmgr[column] * ratio_l
+    df_l = pd.DataFrame(data_l)
+    #
+    # 用货币补足空仓部分， 因为我们的数据库结构无法表示所有资产空
+    # 仓的情况（我们不存储仓位为0的资产）；所以我们需要保证任何一
+    # 天的持仓100%， 如果因为风控空仓，需要用货币补足。
+    #
+    if ratio_h.mean() > 0:
+        sr = ratio_h - df_h.sum(axis=1)
+        if (sr > 0.000099).any():
+            df_h['11310101'] = sr
+
+    if ratio_l.mean() > 0:
+        sr = ratio_l - df_l.sum(axis=1)
+        if (sr > 0.000099).any():
+            df_l['11310100'] = sr
+
+    #
+    # 合并持仓
+    #
+    df = pd.concat([df_h, df_l], axis=1)
+    df = df.T
+    df = df.groupby(df.index).sum()
+    df = df.T
+
+    return df
+
 
 def yao(highlow, alloc):
 
@@ -721,17 +982,17 @@ def nav(ctx, optid, opttype, optlist, optenddate):
 
 def nav_update_alloc(highlow, enddate):
     df_alloc = asset_mz_highlow_alloc.where_highlow_id(highlow['globalid'])
-    
+
     with click.progressbar(length=len(df_alloc), label=('update nav %s' % (highlow['globalid'])).ljust(30)) as bar:
         for _, alloc in df_alloc.iterrows():
             bar.update(1)
             nav_update(alloc, enddate)
-    
+
 def nav_update(alloc, enddate):
     alloc_id = alloc['globalid']
     # 加载仓位信息
     df_pos = asset_mz_highlow_pos.load(alloc_id)
-    
+
     # 加载资产收益率
     min_date = df_pos.index.min()
     #max_date = df_pos.index.max()
@@ -741,11 +1002,19 @@ def nav_update(alloc, enddate):
         max_date = (datetime.now() - timedelta(days=1)) # yesterday
 
 
+    # data = {}
+    # for asset_id in df_pos.columns:
+    #     data[asset_id] = load_nav_series(asset_id, begin_date=min_date, end_date=max_date)
+    # df_nav = pd.DataFrame(data).fillna(method='pad')
+    # df_inc  = df_nav.pct_change().fillna(0.0)
+
+
     data = {}
     for asset_id in df_pos.columns:
         data[asset_id] = load_nav_series(asset_id, begin_date=min_date, end_date=max_date)
     df_nav = pd.DataFrame(data).fillna(method='pad')
     df_inc  = df_nav.pct_change().fillna(0.0)
+
 
     # 计算复合资产净值
     df_nav_portfolio = DFUtil.portfolio_nav(df_inc, df_pos, result_col='portfolio')
@@ -780,13 +1049,13 @@ def turnover(ctx, optid, opttype, optlist):
         df_highlow = asset_mz_highlow.load(highlows)
     else:
         df_highlow = asset_mz_highlow.load(highlows, xtypes)
-        
+
     if optlist:
 
         df_highlow['mz_name'] = df_highlow['mz_name'].map(lambda e: e.decode('utf-8'))
         print tabulate(df_highlow, headers='keys', tablefmt='psql')
         return 0
-    
+
     data = []
     with click.progressbar(length=len(df_highlow), label='update turnover'.ljust(30)) as bar:
         for _, highlow in df_highlow.iterrows():
@@ -919,7 +1188,7 @@ def copy(ctx, optsrc, optdst, optlist):
     df_xtab = df_highlow_alloc[['globalid', 'old']].copy()
 
     df_highlow_alloc.drop(['old'], axis=1, inplace=True)
-    
+
     df_highlow_alloc.set_index(['globalid'], inplace=True)
     asset_mz_highlow_alloc.save(optdst, df_highlow_alloc)
 
@@ -949,4 +1218,4 @@ def copy(ctx, optsrc, optdst, optlist):
     df_highlow_asset = df_highlow_asset.set_index(['mz_highlow_id', 'mz_asset_id'])
 
     asset_mz_highlow_asset.save(df_xtab['globalid'], df_highlow_asset)
-    
+
