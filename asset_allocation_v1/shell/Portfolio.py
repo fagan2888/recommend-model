e5c1020050ae61a6d5fab99984f16226f02c0f21
yjiaoneal <yjiaoneal@163.com>
2018-05-21 19:33:07 +0800

    修正finalws是list的bug

--- asset_allocation_v1/shell/Portfolio.py
+++ asset_allocation_v1/shell/Portfolio.py
@@ -24,29 +24,6 @@ from util.xdebug import dd
 
 logger = logging.getLogger(__name__)
 
-def markowitz_r_spe_volatility_adjusted(funddfr,vol_df, bounds):
-
-    rf = Const.rf
-
-    final_risk = 0
-    final_return = 0
-    final_ws = []
-    final_sharp = -10000000000000000000000000.0
-    final_codes = []
-
-
-    risks, returns, ws = fin.efficient_frontier_spe_volatility_adjusted(funddfr, vol_df, bounds)
-
-    for j in range(0, len(risks)):
-        sharp = (returns[j] - rf) / risks[j]
-        if sharp > final_sharp:
-            final_risk = risks[j]
-            final_return = returns[j]
-            final_ws = ws[j]
-            final_sharp = sharp
-
-    return final_risk, final_return, final_ws, final_sharp
-
 def markowitz_r_spe(funddfr, bounds):
 
     rf = Const.rf
@@ -99,7 +76,7 @@ def markowitz_r_spe_bl(funddfr, P, eta, alpha, bounds):
 
     final_risk = 0
     final_return = 0
-    final_ws = []
+    final_ws = np.array([])
     final_sharp = -10000000000000000000000000.0
     final_codes = []
 
