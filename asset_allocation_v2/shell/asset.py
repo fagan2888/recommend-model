25733a4570eb1fdaa5593bc6a5d68559b3d40cd4
yjiaoneal <yjiaoneal@163.com>
2018-07-10 08:28:55 +0800

    add code

--- asset_allocation_v2/shell/asset.py
+++ asset_allocation_v2/shell/asset.py
@@ -372,10 +372,11 @@ class StockAsset(Asset):
 
 if __name__ == '__main__':
 
-    asset = Asset('120000011')
+    asset = Asset('120000001')
     nav = asset.nav(reindex = ATradeDate.week_trade_date())
     inc = nav.pct_change().dropna()
     print(inc.std())
+    print(np.mean(inc ** 2) ** 0.5)
     #print asset.origin_nav_sr.head()
 
     # asset = WaveletAsset('120000013', 2)
