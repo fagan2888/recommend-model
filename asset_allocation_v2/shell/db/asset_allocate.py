2b43eea6003fb65c9f3eb45b8b8f91a1812c09a8
yjiaoneal <yjiaoneal@163.com>
2018-08-03 18:20:39 +0800

    不同时间资产配置不同上下限

---
+++ asset_allocation_v2/shell/db/asset_allocate.py
@@ -0,0 +1,69 @@
+#coding=utf8
+
+
+import sys
+sys.path.append('shell')
+from sqlalchemy import MetaData, Table, select, func, and_
+from sqlalchemy import Column, String, Integer, ForeignKey, Text, Date, DateTime, Float
+import pandas as pd
+import MySQLdb
+import logging
+from . import database
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dateutil.parser import parse
+import time
+import asset
+import numpy as np
+from functools import reduce
+from ipdb import set_trace
+
+logger = logging.getLogger(__name__)
+
+Base = declarative_base()
+
+class mz_markowitz_bounds(Base):
+
+    __tablename__ = 'mz_markowitz_bounds'
+
+    globalid = Column(String, primary_key = True)
+    mz_asset_id = Column(String)
+    mz_asset_name = Column(String)
+    mz_allocate_start_date = Column(Date)
+    mz_allocate_end_date = Column(Date)
+    mz_upper_limit = Column(Float)
+    mz_lower_limit = Column(Float)
+    mz_sum1_limit = Column(Float)
+    mz_sum2_limit = Column(Float)
+    mz_lower_sum1_limit = Column(Float)
+    mz_lower_sum2_limit = Column(Float)
+    mz_upper_sum1_limit = Column(Float)
+    mz_upper_sum2_limit = Column(Float)
+
+    updated_at = Column(DateTime)
+    created_at = Column(DateTime)
+
+
+def load_mz_markowitz_bounds(globalid):
+
+    engine = database.connection('asset')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    sql = session.query(mz_markowitz_bounds.mz_asset_id, mz_markowitz_bounds.mz_asset_name, mz_markowitz_bounds.mz_allocate_start_date,
+            mz_markowitz_bounds.mz_allocate_end_date, mz_markowitz_bounds.mz_upper_limit, mz_markowitz_bounds.mz_lower_limit,
+            mz_markowitz_bounds.mz_sum1_limit, mz_markowitz_bounds.mz_sum2_limit, mz_markowitz_bounds.mz_lower_sum1_limit, mz_markowitz_bounds.mz_lower_sum2_limit,
+            mz_markowitz_bounds.mz_upper_sum1_limit, mz_markowitz_bounds.mz_upper_sum2_limit,
+            ).filter(mz_markowitz_bounds.globalid == globalid)
+    df = pd.read_sql(sql.statement, session.bind, index_col = ['mz_asset_id'], parse_dates = ['mz_allocate_start_date','mz_allocate_end_date'])
+    session.commit()
+    session.close()
+
+    return df
+
+
+
+
+if __name__ == '__main__':
+
+    df = load_mz_markowitz_bounds('AB.000001')
+    print(df.tail())
