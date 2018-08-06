24ee385557e6f575a71f5843d21580ac81f8ac25
yaojiahui <xzcbvnz@163.com>
2018-08-06 19:09:20 +0800

    add code

---
+++ asset_allocation_v2/fund_pool_valid.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool fund_factor_pool --id 11112001
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool fund_factor_pool --id 11112005
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool fund_factor_pool --id 11112007
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool fund_factor_pool --id 11113001
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool fund_factor_pool --id 11114009
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool fund_factor_pool --id 11114012
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool fund_factor_pool --id 11114018
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool fund_factor_pool --id 11114019
+
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool nav --id 11112001
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool nav --id 11112005
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool nav --id 11112007
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool nav --id 11113001
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool nav --id 11114009
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool nav --id 11114012
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool nav --id 11114018
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool nav --id 11114019
+
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool turnover --id 11112001
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool turnover --id 11112005
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool turnover --id 11112007
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool turnover --id 11113001
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool turnover --id 11114009
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool turnover --id 11114012
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool turnover --id 11114018
+python3 /home/yaojiahui/recommend_model2/recommend_model/asset_allocation_v2/shell/roboadvisor.py pool turnover --id 11114019
