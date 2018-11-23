0a7d41725d53ad2e3599a2b732d20573793fced8
yaojh1994 <xzcbvnz@163.com>
2018-11-23 17:41:28 +0800

    fix bug

--- asset_allocation_v2/cppi_bash.sh
+++ asset_allocation_v2/cppi_bash.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# historyTime_1m=$(date "+%Y-%m-%d" -d '1 month ago')
-# historyTime_3m=$(date "+%Y-%m-%d" -d '3 month ago')
-# historyTime_6m=$(date "+%Y-%m-%d" -d '6 month ago')
-# historyTime_12m=$(date "+%Y-%m-%d" -d '12 month ago')
+historyTime_1m=$(date "+%Y-%m-%d" -d '1 month ago')
+historyTime_3m=$(date "+%Y-%m-%d" -d '3 month ago')
+historyTime_6m=$(date "+%Y-%m-%d" -d '6 month ago')
+historyTime_12m=$(date "+%Y-%m-%d" -d '12 month ago')
 
-# python shell/roboadvisor.py markowitz --id MZ.CB0010 --new --start-date $historyTime_1m
-# python shell/roboadvisor.py highlow --id HL.CB0010 --new
-# python shell/roboadvisor.py portfolio --id PO.CB0010 --new
+python shell/roboadvisor.py markowitz --id MZ.CB0010 --new --start-date $historyTime_1m
+python shell/roboadvisor.py highlow --id HL.CB0010 --new
+python shell/roboadvisor.py portfolio --id PO.CB0010 --new
 
-# python shell/roboadvisor.py markowitz --id MZ.CB0020 --new --start-date $historyTime_3m
-# python shell/roboadvisor.py highlow --id HL.CB0020 --new
-# python shell/roboadvisor.py portfolio --id PO.CB0020 --new
+python shell/roboadvisor.py markowitz --id MZ.CB0020 --new --start-date $historyTime_3m
+python shell/roboadvisor.py highlow --id HL.CB0020 --new
+python shell/roboadvisor.py portfolio --id PO.CB0020 --new
 
-# python shell/roboadvisor.py markowitz --id MZ.CB0030 --new --start-date $historyTime_6m
-# python shell/roboadvisor.py highlow --id HL.CB0030 --new
-# python shell/roboadvisor.py portfolio --id PO.CB0030 --new
+python shell/roboadvisor.py markowitz --id MZ.CB0030 --new --start-date $historyTime_6m
+python shell/roboadvisor.py highlow --id HL.CB0030 --new
+python shell/roboadvisor.py portfolio --id PO.CB0030 --new
 
-# python shell/roboadvisor.py markowitz --id MZ.CB0040 --new --start-date $historyTime_12m
-# python shell/roboadvisor.py highlow --id HL.CB0040 --new
-# python shell/roboadvisor.py portfolio --id PO.CB0040 --new
+python shell/roboadvisor.py markowitz --id MZ.CB0040 --new --start-date $historyTime_12m
+python shell/roboadvisor.py highlow --id HL.CB0040 --new
+python shell/roboadvisor.py portfolio --id PO.CB0040 --new
 
-python shell/roboadvisor.py composite nav --asset PO.CB0010
-python shell/roboadvisor.py composite nav --asset PO.CB0020
-python shell/roboadvisor.py composite nav --asset PO.CB0030
-python shell/roboadvisor.py composite nav --asset PO.CB0040
+python shell/roboadvisor.py composite nav --asset 20301
+python shell/roboadvisor.py composite nav --asset 20302
+python shell/roboadvisor.py composite nav --asset 20303
+python shell/roboadvisor.py composite nav --asset 20304
