cd1ba731ba22b861e0bed1a8dc14bcb941cc4b16
caesious273 <caesious273@sina.com>
2019-01-08 20:13:12 +0800

    20190108-1

--- asset_allocation_v2/shell/CommandFundIncEst.py
+++ asset_allocation_v2/shell/CommandFundIncEst.py
@@ -15,6 +15,7 @@ import click
 import config
 import pandas as pd
 import numpy as np
+import copy
 import time
 from scipy.optimize import minimize
 from scipy.stats import spearmanr
@@ -46,42 +47,60 @@ def fund_inc_estimate_create():
     '''fund inc estimate create
     '''
 
-    fiesp = FundIncEstSkPos(begin_date='20181101')
+    fiesp = FundIncEstSkPos(begin_date='20171201')
     df_fiesp = fiesp.estimate_fund_inc()
     df_fiesp = df_fiesp.stack().to_frame(name='sk_pos')
 
-    fieip = FundIncEstIxPos(begin_date='20181101')
+    fieip = FundIncEstIxPos(begin_date='20171201')
     df_fieip = fieip.estimate_fund_inc()
     df_fieip = df_fieip.stack().to_frame(name='ix_pos')
 
     df_fie = df_fiesp.join(df_fieip, how='outer')
+    df_fie['mix'] = df_fie.mean(axis='columns')
+    df_fie.to_csv('data/df_fie.csv')
+    #df_fie = pd.read_csv(
+    #        'data/df_fie.csv',
+    #        dtype={'fund_code': str},
+    #        parse_dates=['date']
+    #)
+    #df_fie = df_fie.set_index(['date', 'fund_code'])
 
-    asset_fund_inc_estimate.update_fund_inc_estimate(df_fie)
+    asset_fund_inc_estimate.update_fund_inc_estimate(copy.deepcopy(df_fie))
 
-    fiem = FundIncEstMix('20181201')
+    fiem = FundIncEstMix('20180101')
     df_fiem = fiem.estimate_fund_inc()
     df_fiem = df_fiem.stack().to_frame(name='mix')
 
-    df_fie = df_fie.join(df_fiem, how='left')
+    df_fie = df_fie.join(df_fiem, how='left', lsuffix='_old', rsuffix='_new')
+    df_fie['mix'] = df_fie.apply(lambda x: x.mix_old if x.isna().mix_new else x.mix_new, axis='columns')
+    df_fie = df_fie.drop(['mix_old', 'mix_new'], axis='columns')
+    df_fie = df_fie.loc[pd.Timestamp('20180101'):]
 
-    asset_fund_inc_estimate.update_fund_inc_estimate(df_fie, begin_date='20181201')
+    asset_fund_inc_estimate.update_fund_inc_estimate(df_fie)
 
 if 1==1:
     fund_inc_estimate_create()
-
-# 再写一个update_all_methods
-
 set_trace()
 
-@fie.command()
-@click.pass_context
-def fund_inc_estimate_update(ctx):
+# @fie.command()
+# @click.pass_context
+def fund_inc_estimate_update():
     '''fund inc estimate update
     '''
+
+    fiesp = FundIncEstSkPos()
+    df_fiesp = fiesp.estimate_fund_inc()
+    df_fiesp = df_fiesp.stack().to_frame(name='sk_pos')
+
+    fieip = FundIncEstIxPos()
+    df_fieip = fieip.estimate_fund_inc()
+    df_fieip = df_fieip.stack().to_frame(name='ix_pos')
 
     df_fund_inc_estimate = fund_inc_estimate()
     # df_fund_inc_estimate.index.names = []
     # df_fund_inc_estimate.columns = []
     asset_fund_inc_estimate.update_fund_inc_estimate(df_fund_inc_estimate)
 
-
+if 1 == 1:
+    fund_inc_estimate_update()
+set_trace()
