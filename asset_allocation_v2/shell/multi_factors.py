0b43665c7b6198740182a362abfaf063d2c117e0
caesious273 <caesious273@gmail.com>
2019-03-11 00:49:03 +0800

    add code: stock portfolio.

--- asset_allocation_v2/shell/multi_factors.py
+++ asset_allocation_v2/shell/multi_factors.py
@@ -1,6 +1,7 @@
 #coding=utf-8
 '''
 Created on: Mar. 6, 2019
+Modified on: Mar. 11, 2019
 Author: Shixun Su, Boyang Zhou
 Contact: sushixun@licaimofang.com
 '''
@@ -9,11 +10,14 @@ import sys
 import logging
 import click
 import warnings
+# import multiprocessing
+# from multiprocessing import Pooli, Manager
 import numpy as np
 import pandas as pd
 from scipy.stats import norm, poisson, gaussian_kde
 import statsmodels.api as sm
 from statsmodels.distributions.empirical_distribution import ECDF, monotone_fn_inverter
+# import tensorflow as tf
 sys.path.append('shell')
 from db import caihui_tq_ix_comp, caihui_tq_sk_basicinfo, caihui_tq_sk_dquoteindic, caihui_tq_sk_finindic
 from trade_date import ATradeDate
@@ -24,31 +28,73 @@ from ipdb import set_trace
 logger = logging.getLogger(__name__)
 
 
-def load_CSI800():
 
-    # df_CSI800_pool = caihui_tq_ix_comp.load_index_constituents('2070000191')
-    df_CSI800_constituents = pd.read_csv('data/CSI800_constituents.csv')
-    df_CSI800_pool = caihui_tq_sk_basicinfo.load_stock_code_info(stock_codes=df_CSI800_constituents['Constituents Code'], current=False)
+class MultiFators(object):
 
-    df_CSI800_market_data = caihui_tq_sk_dquoteindic.load_stock_market_data(begin_date='20000101', stock_ids=df_CSI800_pool.index)
-    df_CSI800_financial_data = caihui_tq_sk_finindic.load_stock_financial_data(begin_date='20000101', stock_ids=df_CSI800_pool.index)
-    df_CSI800_industry = caihui_tq_sk_basicinfo.load_stock_industry(stock_ids=df_CSI800_pool.index)
-    set_trace()
-    result = pd.merge(df_CSI800_dquote_indic, df_CSI800_financial_data, how='outer', on=['stock_id', 'date'])
+    def __init__(self, index_id='2070000191', begin_date='20000101'):
 
-    set_trace()
+        self.__stock_pool = caihui_tq_ix_comp.load_index_constituents(index_id)
+        # df_CSI800_constituents = pd.read_csv('data/CSI800_constituents.csv')
+        # df_CSI800_pool = caihui_tq_sk_basicinfo.load_stock_code_info(stock_codes=df_CSI800_constituents['Constituents Code'], current=False)
 
-def GLS(Y, X):
+        self._df_stock_nav = caihui_tq_sk_dquoteindic.load_stock_nav(begin_date=begin_date, stock_ids=self.__stock_pool.index)
+        self._df_stock_ret = self._df_stock_nav.pct_change().iloc[1:]
 
-    index = X.index.intersection(Y.index)
-    X = X.reindex(index)
-    Y = Y.reindex(index)
+        # self._df_stock_market_data = caihui_tq_sk_dquoteindic.load_stock_market_data(begin_date=begin_date, stock_ids=self.__stock_pool.index)
+        # self._df_stock_financial_data = caihui_tq_sk_finindic.load_stock_financial_data(begin_date=begin_date, stock_ids=self.__stock_pool.index)
+        # self._df_stock_industry = caihui_tq_sk_basicinfo.load_stock_industry(stock_ids=self.__stock_pool.index)
+        self._df_stock_indicators = self._cal_indicators()
 
-    GLS = sm.GLS(Y, X)
-    GLS_results = GLS.fit()
-    print(GLS_results.summary())
+    @property
+    def stock_pool(self):
+        return self.__stock_pool
 
-    return GLS_results
+    def _cal_indicators(self, window=120):
+
+        df = pd.DataFrame()
+        df['volatility'] = self._df_stock_ret.rolling(window=window).std().iloc[window:].unstack().dropna()
+
+        return df
+
+    def _cal_weight(self, factor, method='daoshu'):
+
+        if method == 'daoshu':
+
+            factor = factor.unstack().T
+            weight = factor.applymap(lambda x: 1/x if x>0.0 else np.nan)
+            weight = weight.apply(lambda x: x/x.sum(), axis='columns')
+            set_trace()
+
+    def cal_ret_list(self):
+
+        df_weight = self._cal_weight(self._df_stock_indicators.volatility)
+        ser_portfolio_ret = (df_weight * self._df_stock_ret).sum(axis='columns')
+
+        return ser_portfolio_ret
+
+    def analysis(self, ser_portfolio_ret):
+
+        ret = (ser_portfolio_ret + 1).pi - 1
+        risk = ser_portfolio_ret.std()
+        sharpe_ratio = ret / risk
+
+        print(ret, risk, sharpe_ratio)
+
+
+    def GLS(self, Y, X):
+
+        index = X.index.intersection(Y.index)
+        X = X.reindex(index)
+        Y = Y.reindex(index)
+
+        GLS = sm.GLS(Y, X)
+        GLS_results = GLS.fit()
+        print(GLS_results.summary())
+
+        return GLS_results
+
+
+
 
 # from db import caihui_tq_qt_index
 # Ret_indexing = caihui_tq_qt_index.load_index_nav(begin_date='20100101', index_ids=['2070000191']).pct_change().iloc[1:]
@@ -63,8 +109,8 @@ def GLS(Y, X):
 
 # GLS(Ret_indexing,PS_ratio.mean(axis=1))
 
-
 if __name__ == '__main__':
 
-    load_CSI800()
+    mf = MultiFators(begin_date='20160101')
+    x = mf.cal_ret_list()
 
