fbef941ae11e7836cb90fb9cf37d3d7c207b2d51
yangning1994 <2017210261@email.cufe.edu.cn>
2019-05-21 12:19:19 +0800

    change calc_financial_descriptor

--- asset_allocation_v2/shell/calc_financial_descriptor.py
+++ asset_allocation_v2/shell/calc_financial_descriptor.py
@@ -1,105 +1,81 @@
+#coding=utf-8
 '''
-Created on: May. 7, 2019
+Created on: May. 19, 2019
 Author: Ning Yang
 Contact: yangning@licaimofang.com
 '''
-# calculate financial indicator ---> calculate TTM indicator ---> calculate TTM descriptor --->
-# calculate the growth of TTM descriptor ---> calculate the rolling descriptor
+
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+from db import wind_asharecalendar, wind_asharebalancesheet, wind_ashareincome, wind_asharecashflow, wind_aindexmembers 
+from GenerateStockFactorTools import *
+from ipdb import set_trace
 
+def calc_stock_financial_descriptor(stock_ids_total):
+    
+    df_BS = wind_asharebalancesheet.load_a_stock_balancesheet(stock_ids=stock_ids_total)
+    df_IS = wind_ashareincome.load_a_stock_income(stock_ids=stock_ids_total)  
+    df_CF = wind_asharecashflow.load_a_stock_cashflow(stock_ids=stock_ids_total)
+    
+    df_FS = pd.merge(df_BS, df_IS, how='left', on=['stock_id', 'report_period'])
+    df_FS = pd.merge(df_FS, df_CF, how='left', on=['stock_id', 'report_period'])
+    
+    df_FS['closing_date'] = df_FS.report_period.map(lambda x: closing_dt(x))
+    df_FS.loc[df_FS.ann_date.isna(), 'ann_date'] = df_FS.loc[df_FS.ann_date.isna(), 'closing_date'] #尚未测试
+    del df_FS['closing_date']
 
-def closing_dt(x):
-    if x.month == 3:
-        return pd.Timestamp(x.year, 4, 30)
-    elif x.month == 6:
-        return pd.Timestamp(x.year, 8, 31)
-    elif x.month == 9:
-        return pd.Timestamp(x.year, 10, 31)
-    elif x.month == 12:
-        return pd.Timestamp(x.year+1, 4, 30)
-    else:
-        return pd.Timestamp(1990, 1, 1)  # 特殊标记
+    df_FS['wc_cf'] = df_FS[['depr_fa_coga_dpba', 'amort_intang_assets', 'amort_lt_deferred_exp', 'others']].sum(axis=1, skipna=True, min_count=1)
+    df_FS['d_a'] = df_FS[['depr_fa_coga_dpba', 'amort_intang_assets', 'amort_lt_deferred_exp']].sum(axis=1, skipna=True, min_count=1)
 
+    df_FS['lfy'] = df_FS.report_period.map(lambda x: pd.Timestamp(x.year - 1, 12, 31))
+    df_FS['comp'] = df_FS.report_period.map(lambda x: pd.Timestamp(x.year - 1, x.month, x.day))
+    df_FS_t = df_FS.copy()
+    df_FS = pd.merge(df_FS, df_FS, how='left', left_on=['stock_id', 'lfy'], right_on=['stock_id', 'report_period'], suffixes=('', '_lfy'))
+    df_FS = pd.merge(df_FS, df_FS_t, how='left', left_on=['stock_id', 'comp'], right_on=['stock_id', 'report_period'], suffixes=('', '_comp'))
+    
+    columns_ttm = ['oper_rev', 'less_oper_cost', 'net_profit_after_ded_nr_lp', 'net_cash_flows_oper_act', 'wc_cf', 'd_a']
+    for i_ttm in columns_ttm:
+        df_FS[i_ttm+'_ttm'] = (df_FS[i_ttm] - df_FS[i_ttm + '_comp']).add(df_FS[i_ttm + '_lfy'], fill_value=0)
 
-def _lt_growth(data):
-    data_t = data[~np.isnan(data)].copy()
-    x = np.arange(data_t.shape[0]) + 1
-    y = data_t
-    ols_results = sm.OLS(y, sm.add_constant(x)).fit()
-    growth_t = ols_results.params[1] / np.mean(data_t)
-    return growth_t
+    set_trace()
+    df_FS['roa'] = 2 * df_FS['net_cash_flows_oper_act_ttm'] / (df_FS['tot_assets'] + df_FS['tot_assets_comp'])
+    df_FS['roe'] = 2 * df_FS['net_profit_after_ded_nr_lp_ttm'] / (df_FS['net_profit_after_ded_nr_lp'] + df_FS['net_profit_after_ded_nr_lp_ttm'])
+    df_FS['cash_flow_accrual'] = 2 * (df_FS['wc_cf_ttm'] - df_FS['d_a_ttm']) / (df_FS['tot_assets'] + df_FS['tot_assets_comp'])
+    df_FS['gross_profit_margin'] = (df_FS['oper_rev_ttm'] - df_FS['less_oper_cost_ttm']) / df_FS['oper_rev_ttm']
+    df_FS['assets_turnover'] = 2 * df_FS['oper_rev_ttm'] / (df_FS['tot_assets'] + df_FS['tot_assets_comp'])
+    df_FS['gross_profit'] = df_FS['oper_rev_ttm'] - df_FS['less_oper_cost_ttm']
 
+    df_FS = pd.merge(df_FS, df_FS, how='left', left_on=['stock_id', 'comp'], right_on=['stock_id', 'report_period'], suffixes=('', '_comp'))
+    df_FS['earnings_growth'] = df_FS['net_profit_after_ded_nr_lp_ttm'] / np.abs(df_FS['net_profit_after_ded_nr_lp_ttm_comp']) - 1
+    df_FS['ocf_growth'] = df_FS['net_cash_flows_oper_act_ttm'] / np.abs(df_FS['net_cash_flows_oper_act_ttm_comp']) - 1
+    df_FS['gross_profit_growth'] = df_FS['gross_profit'] / np.abs(df_FS['gross_profit_comp']) - 1
+    df_FS['gross_profit_margin_growth'] = df_FS['gross_profit_margin'] / np.abs(df_FS['gross_profit_margin_comp']) - 1
+    df_FS['assets_turnover_growth'] = df_FS['assets_turnover'] / np.abs(df_FS['assets_turnover_comp']) - 1
+    
+    set_trace()
 
-def _e_vol(data):
-    data_t = data[~np.isnan(data)].copy()
-    growth_list = list()
-    for i in range(1, data_t.shape[0]):
-        if data_t[i-1] != 0:
-            growth_t = (data_t[i] - data_t[i-1]) / np.abs(data_t[i-1])
-        else:
-            growth_t = 0
-        growth_list.append(growth_t)
-    growth_array = np.array(growth_list)
-    fun_var = np.cov(growth_array)
-    fun_std = np.sqrt(fun_var)
-    return fun_std
-
-
-def calc_financial_descriptor(data):
-    # fill the null actual announce date with closing date
-    data_FS = data.copy()
-    data_FS.REPORT_PERIOD = pd.to_datetime(data_FS.REPORT_PERIOD.astype(str)).astype(pd.Timestamp)
-    loc_t = data_FS.ACTUAL_ANN_DT.isna()
-    data_FS.loc[loc_t, 'ACTUAL_ANN_DT'] = 19900101.00
-    data_FS.ACTUAL_ANN_DT = pd.to_datetime(data_FS.ACTUAL_ANN_DT.astype(int).astype(str)).astype(pd.Timestamp)
-    data_FS['CLOSING_DT'] = data_FS.REPORT_PERIOD.map(lambda x: closing_dt(x))
-    data_FS.loc[loc_t, 'ACTUAL_ANN_DT'] = data_FS.loc[loc_t, 'CLOSING_DT'].astype(pd.Timestamp)
-    # calculate financial indicator
-    data_FS['WC_CF'] = data_FS[['DECR_INVENTORIES', 'DECR_OPER_PAYABLE', 'INCR_OPER_PAYABLE', 'OTHERS']].sum(axis=1, skipna=True, min_count=1)
-    data_FS['D_A'] = data_FS[['DEPR_FA_COGA_DPBA', 'AMORT_INTANG_ASSETS', 'AMORT_LT_DEFERRED_EXP']].sum(axis=1, skipna=True, min_count=1)
-    # merge
-    data_FS['LFY'] = data_FS.REPORT_PERIOD.map(lambda x: pd.Timestamp(x.year - 1, 12, 31)).astype(pd.Timestamp)
-    data_FS['COMP'] = data_FS.REPORT_PERIOD.map(lambda x: pd.Timestamp(x.year - 1, x.month, x.day)).astype(pd.Timestamp)
-    data_FS_t = data_FS.copy()
-    data_FS = pd.merge(data_FS, data_FS, how='left', left_on=['WIND_CODE', 'LFY'], right_on=['WIND_CODE', 'REPORT_PERIOD'], suffixes=('', '_LFY'))
-    data_FS = pd.merge(data_FS, data_FS_t, how='left', left_on=['WIND_CODE', 'COMP'], right_on=['WIND_CODE', 'REPORT_PERIOD'], suffixes=('', '_COMP'))
-    columns_del = ['ACTUAL_ANN_DT_LFY', 'REPORT_PERIOD_LFY', 'LFY_LFY', 'COMP_LFY', 'ACTUAL_ANN_DT_COMP', 'REPORT_PERIOD_COMP', 'LFY_COMP', 'COMP_COMP']
-    data_FS.drop(columns_del, axis=1, inplace=True)
-    # calculate TTM indicator
-    descriptor_TTM = ['OPER_REV', 'LESS_OPER_COST', 'NET_PROFIT_AFTER_DED_NR_LP', 'NET_CASH_FLOWS_OPER_ACT', 'WC_CF', 'D_A']
-    for i_ttm in descriptor_TTM:
-        data_FS[i_ttm + '_TTM'] = (data_FS[i_ttm] - data_FS[i_ttm + '_COMP']).add(data_FS[i_ttm + '_LFY'], fill_value=0)
-    # calculate TTM descriptor
-    data_FS['QUALITY_ROA'] = 2 * data_FS['NET_CASH_FLOWS_OPER_ACT_TTM'] / (data_FS['TOT_ASSETS'] + data_FS['TOT_ASSETS_COMP'])
-    data_FS['QUALITY_ROE'] = 2 * data_FS['NET_PROFIT_AFTER_DED_NR_LP_TTM'] / (data_FS['TOT_SHRHLDR_EQY_EXCL_MIN_INT'] + data_FS['TOT_SHRHLDR_EQY_EXCL_MIN_INT_COMP'])
-    data_FS['QUALITY_ACCF'] = 2 * (data_FS['WC_CF_TTM'] - data_FS['D_A_TTM']) / (data_FS['TOT_ASSETS'] + data_FS['TOT_ASSETS_COMP'])
-    data_FS['QUALITY_GPM'] = (data_FS['OPER_REV_TTM'] - data_FS['LESS_OPER_COST_TTM']) / data_FS['OPER_REV_TTM']
-    data_FS['ATO'] = 2 * data_FS['OPER_REV_TTM'] / (data_FS['TOT_ASSETS'] + data_FS['TOT_ASSETS_COMP'])
-    data_FS['GP'] = data_FS['OPER_REV_TTM'] - data_FS['LESS_OPER_COST_TTM']
-    # calculate the growth of TTM descriptor
-    data_FS = pd.merge(data_FS, data_FS, how='left', left_on=['WIND_CODE', 'COMP'], right_on=['WIND_CODE', 'REPORT_PERIOD'], suffixes=('', '_COMP'))
-    data_FS['GROWTH_EGRO'] = data_FS['NET_PROFIT_AFTER_DED_NR_LP_TTM'] / np.abs(data_FS['NET_PROFIT_AFTER_DED_NR_LP_TTM_COMP']) - 1
-    data_FS['GROWTH_CGRO'] = data_FS['NET_CASH_FLOWS_OPER_ACT_TTM'] / np.abs(data_FS['NET_CASH_FLOWS_OPER_ACT_TTM_COMP']) - 1
-    data_FS['GROWTH_GPGRO'] = data_FS['GP'] / np.abs(data_FS['GP_COMP']) - 1
-    data_FS['GROWTH_GPMGRO'] = data_FS['QUALITY_GPM'] / np.abs(data_FS['QUALITY_GPM_COMP']) - 1
-    data_FS['GROWTH_ATOGRO'] = data_FS['ATO'] / np.abs(data_FS['ATO_COMP']) - 1
-    # calculate the rolling descriptor
-    columns_add = ['QUALITY_VERN', 'QUALITY_AGRO']
+    columns_add = ['earnings_volatility', 'assets_growth']
     for i_add in columns_add:
-        data_FS[i_add] = np.nan
-    data_FS = data_FS.set_index('WIND_CODE').sort_values(by=['WIND_CODE', 'REPORT_PERIOD'], ascending=True)
-    for i_code in data_FS.index.unique():
-        data_FS.loc[i_code, ['QUALITY_VERN']] = data_FS.loc[i_code, ['NET_PROFIT_AFTER_DED_NR_LP_TTM']].rolling(20, min_periods=12).apply(lambda x: _e_vol(x), 'raw=True').values
-        data_FS.loc[i_code, ['QUALITY_AGRO']] = data_FS.loc[i_code, ['TOT_ASSETS']].rolling(20, min_periods=12).apply(lambda x: _lt_growth(x), 'raw=True').values
+        df_FS[i_add] = np.nan
+    
+    df_FS = df_FS.set_index('stock_id').sort_values(by=['stock_id', 'report_period'])
+    
+    for i_stock in df_FS.index.unique():
+        df_FS.loc[i_stock, ['earnings_volatility']] = df_FS.loc[i_stock, ['net_profit_after_ded_nr_lp_ttm']]\
+            .rolling(20, min_periods=12).apply(lambda x: e_vol(x), 'raw=True').values
+        df_FS.loc[i_stock, ['assets_growth']] = df_FS.loc[i_stock, ['tot_assets']]\
+            .rolling(20, min_periods=12).apply(lambda x: lt_growth(x), 'raw=True').values
+    
+    df_FS.reset_index(inplace=True)
+    columns_retain = [
+        'stock_id', 'ann_date', 'report_period', 'roa', 'roe', 'cash_flow_accrual', 'gross_profit_margin', 'assets_turnover','earnings_growth', 
+        'ocf_growth', 'gross_profit_growth', 'gross_profit_margin_growth', 'assets_turnover_growth', 'earnings_volatility', 'assets_growth'
+    ]
+    set_trace()
+    return df_FS[columns_retain]
 
-    data_FS.reset_index(inplace=True)
-    columns_retain = ['WIND_CODE', 'stock_id', 'ACTUAL_ANN_DT', 'REPORT_PERIOD', 'QUALITY_ROA', 'QUALITY_ROE',
-                      'QUALITY_ACCF', 'QUALITY_GPM', 'QUALITY_VERN', 'QUALITY_AGRO', 'GROWTH_EGRO', 'GROWTH_CGRO',
-                      'GROWTH_GPGRO', 'GROWTH_GPMGRO', 'GROWTH_ATOGRO']
-    columns_negative = ['QUALITY_VERN', 'QUALITY_AGRO']
-    data_FS[columns_negative] = - data_FS[columns_negative]
-    stock_financial_descriptor = data_FS[columns_retain].copy()
-    return stock_financial_descriptor
-
+if __name__ == '__main__':
+    df_index_historical_constituents = wind_aindexmembers.load_a_index_historical_constituents(index_id='000985.CSI')
+    stock_ids_total = df_index_historical_constituents.stock_id.unique()
+    df = calc_stock_financial_descriptor(stock_ids_total=stock_ids_total)
